07451ddb90698152546b5b1cf5f061c00c0f7efd
Dave McKinstry <dmckinstry@users.noreply.github.com>
2021-10-16 15:46:20 +0000

    Fix CodeQL report and enable Test Explorer

--- src/OctoshiftCLI.Tests/Helpers.cs
+++ src/OctoshiftCLI.Tests/Helpers.cs
@@ -1,25 +1,19 @@
 using System;
 using System.CommandLine;
-//using System.CommandLine.Invocation;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace OctoshiftCLI.Tests.Commands
 {
     public class Helpers
     {
-        //
         public static void VerifyCommandOption(IReadOnlyList<Option> options, string name, bool required)
         {
-            foreach (var option in options)
-            {
-                if (option.Name == name)
-                {
-                    Assert.Equal(required, option.IsRequired);
-                    return;
-                }
-            }
-            Assert.True(false, $"Option '{name}' not found");
+            var option = options.First(x => x.Name == name);
+            
+            Assert.True(option != null, $"Option {name} not found");
+            Assert.Equal(required, option.IsRequired);
         }
     }
 }
