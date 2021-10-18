ff5dcd0a155191d9fdd6f6cfff8e560726038a54
Rogan Ferguson <roferg@github.com>
2021-10-18 15:08:04 -0500

    Adding grant and revoke migrator role commands

---
+++ src/OctoshiftCLI/Commands/RevokeMigratorRole.cs
@@ -0,0 +1,86 @@
+using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OctoshiftCLI.Commands
+{
+    public class RevokeMigratorRoleCommand : Command
+    {
+        private GithubApi _github;
+
+        public RevokeMigratorRoleCommand() : base("revoke-migrator-role")
+        {
+            var githubOrg = new Option<string>("--github-org")
+            {
+                IsRequired = true
+            };
+            var actor = new Option<string>("--actor")
+            {
+                IsRequired = true
+            };
+            var actorType = new Option<string>("--actor-type")
+            {
+                IsRequired = true
+            };
+            
+            AddOption(githubOrg);
+            AddOption(actor);
+            AddOption(actorType);
+
+            Handler = CommandHandler.Create<string, string, string>(Invoke);
+        }
+
+        private async Task Invoke(string githubOrg, string actor, string actorType)
+        {
+            Console.WriteLine("Granting migrator role ...");
+            Console.WriteLine($"GITHUB ORG: {githubOrg}");
+            Console.WriteLine($"ACTOR: {actor}");
+
+            actorType = actorType.ToUpper();
+            Console.WriteLine($"ACTOR TYPE: {actorType}");
+
+            var githubToken = Environment.GetEnvironmentVariable("GH_PAT");
+            actorType = actorType.ToUpper();
+
+            if (string.IsNullOrWhiteSpace(githubToken))
+            {
+                Console.WriteLine("ERROR: NO GH_PAT FOUND IN ENV VARS, exiting...");
+                return;
+            }
+
+            if(actorType == "TEAM" || actorType == "USER")
+            {
+               Console.WriteLine("Actor type is valid...");
+            } 
+            else 
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERROR: Actor type must be either TEAM or USER.");
+                Console.ResetColor();
+                return;
+            }
+
+            _github = new GithubApi(githubToken);
+
+            var githubOrgId = await _github.GetOrganizationId(githubOrg);
+            var revokeMigratorRoleState = await _github.RevokeMigratorRole(githubOrgId, actor, actorType);
+
+            Console.WriteLine(revokeMigratorRoleState);
+
+            if (revokeMigratorRoleState.Trim().ToUpper() == "TRUE")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"SUCCESS: Migrator role successfully revoked for the {actorType} \"{actor}\"");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"ERROR: Migrator role couldn't be revoked for the {actorType} \"{actor}\"");
+                Console.ResetColor();
+            }
+        }
+    }
+}
