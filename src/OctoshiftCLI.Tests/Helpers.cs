33b1ac7c24849f44da28abab3c23904dbe0256be
Dave McKinstry <dmckinstry@users.noreply.github.com>
2021-10-19 22:50:01 +0000

    Comment out incomplete code

--- src/OctoshiftCLI.Tests/Helpers.cs
+++ src/OctoshiftCLI.Tests/Helpers.cs
@@ -17,53 +17,53 @@ namespace OctoshiftCLI.Tests
             Assert.Equal(required, option.IsRequired);
         }
 
+        //TODO: Complete in support of integration tests
+        // // NOTE: The following method is derived from: https://jackma.com/2019/04/20/execute-a-bash-script-via-c-net-core/
+        // public static Task<int> Bash(string cwd, string cmd)
+        // {
+        //     var source = new TaskCompletionSource<int>();
+        //     var escapedArgs = cmd.Replace("\"", "\\\"");
+        //     var process = new Process
+        //     {
+        //         StartInfo = new ProcessStartInfo
+        //         {
+        //             FileName = "bash",
+        //             Arguments = $"-c \"{escapedArgs}\"",
+        //             RedirectStandardOutput = true,
+        //             RedirectStandardError = true,
+        //             WorkingDirectory = cwd,
+        //             UseShellExecute = false,
+        //             CreateNoWindow = true
+        //         },
+        //         EnableRaisingEvents = true
+        //     };
 
-        // NOTE: The following method is derived from: https://jackma.com/2019/04/20/execute-a-bash-script-via-c-net-core/
-        public static Task<int> Bash(string cwd, string cmd)
-        {
-            var source = new TaskCompletionSource<int>();
-            var escapedArgs = cmd.Replace("\"", "\\\"");
-            var process = new Process
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = "bash",
-                    Arguments = $"-c \"{escapedArgs}\"",
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    WorkingDirectory = cwd,
-                    UseShellExecute = false,
-                    CreateNoWindow = true
-                },
-                EnableRaisingEvents = true
-            };
+        //     process.Exited += (sender, args) =>
+        //     {
+        //         Debug.WriteLine(process.StandardOutput.ReadToEnd());
+        //         Console.WriteLine(process.StandardError.ReadToEnd());
+        //         if (process.ExitCode == 0)
+        //         {
+        //             source.SetResult(0);
+        //         }
+        //         else
+        //         {
+        //             source.SetException(new Exception($"Command `{cmd}` failed with exit code `{process.ExitCode}`"));
+        //         }
 
-            process.Exited += (sender, args) =>
-            {
-                Debug.WriteLine(process.StandardOutput.ReadToEnd());
-                Console.WriteLine(process.StandardError.ReadToEnd());
-                if (process.ExitCode == 0)
-                {
-                    source.SetResult(0);
-                }
-                else
-                {
-                    source.SetException(new Exception($"Command `{cmd}` failed with exit code `{process.ExitCode}`"));
-                }
+        //         process.Dispose();
+        //     };
 
-                process.Dispose();
-            };
+        //     try
+        //     {
+        //         process.Start();
+        //     }
+        //     catch (Exception e)
+        //     {
+        //         Console.WriteLine($"Command {cmd} failed with {e.Message}");
+        //     }
 
-            try
-            {
-                process.Start();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Command {cmd} failed with {e.Message}");
-            }
-
-            return source.Task;
-        }
+        //     return source.Task;
+        // }
     }
 }
