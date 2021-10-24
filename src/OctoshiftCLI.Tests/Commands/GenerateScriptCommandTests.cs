68c9d0cf117f437fe8bee027ed5f85e37600a0b7
Dylan Smith <dylan-smith@github.com>
2021-10-23 23:14:32 -0500

    some command option tests were unfinished (#74)

--- src/OctoshiftCLI.Tests/Commands/GenerateScriptCommandTests.cs
+++ src/OctoshiftCLI.Tests/Commands/GenerateScriptCommandTests.cs
@@ -13,24 +13,11 @@ namespace OctoshiftCLI.Tests.Commands
             Assert.Equal("generate-script", command.Name);
             Assert.Equal(5, command.Options.Count);
 
-            // TODO: This looks wrong
             TestHelpers.VerifyCommandOption(command.Options, "github-org", true);
-            TestHelpers.VerifyCommandOption(command.Options, "github-org", true);
-            TestHelpers.VerifyCommandOption(command.Options, "github-org", true);
-            TestHelpers.VerifyCommandOption(command.Options, "github-org", true);
-            TestHelpers.VerifyCommandOption(command.Options, "github-org", true);
-
-
-            Assert.Equal("github-org", command.Options[0].Name);
-            Assert.True(command.Options[0].IsRequired);
-            Assert.Equal("ado-org", command.Options[1].Name);
-            Assert.False(command.Options[1].IsRequired);
-            Assert.Equal("output", command.Options[2].Name);
-            Assert.False(command.Options[2].IsRequired);
-            Assert.Equal("repos-only", command.Options[3].Name);
-            Assert.False(command.Options[3].IsRequired);
-            Assert.Equal("skip-idp", command.Options[4].Name);
-            Assert.False(command.Options[4].IsRequired);
+            TestHelpers.VerifyCommandOption(command.Options, "ado-org", false);
+            TestHelpers.VerifyCommandOption(command.Options, "output", false);
+            TestHelpers.VerifyCommandOption(command.Options, "repos-only", false);
+            TestHelpers.VerifyCommandOption(command.Options, "skip-idp", false);
         }
     }
 }
