f3a0e3a41b8327b4915c068533061f29267cb193
Dylan Smith <dylan-smith@github.com>
2021-10-23 23:08:33 -0500

    some command option tests were unfinished

--- src/OctoshiftCLI.Tests/Commands/IntegrateBoardsCommandTests.cs
+++ src/OctoshiftCLI.Tests/Commands/IntegrateBoardsCommandTests.cs
@@ -13,22 +13,10 @@ namespace OctoshiftCLI.Tests.Commands
             Assert.Equal("integrate-boards", command.Name);
             Assert.Equal(4, command.Options.Count);
 
-            // TODO: This looks wrong
+            TestHelpers.VerifyCommandOption(command.Options, "ado-org", true);
+            TestHelpers.VerifyCommandOption(command.Options, "ado-team-project", true);
             TestHelpers.VerifyCommandOption(command.Options, "github-org", true);
-            TestHelpers.VerifyCommandOption(command.Options, "github-org", true);
-            TestHelpers.VerifyCommandOption(command.Options, "github-org", true);
-            TestHelpers.VerifyCommandOption(command.Options, "github-org", true);
-
-
-
-            Assert.Equal("ado-org", command.Options[0].Name);
-            Assert.True(command.Options[0].IsRequired);
-            Assert.Equal("ado-team-project", command.Options[1].Name);
-            Assert.True(command.Options[1].IsRequired);
-            Assert.Equal("github-org", command.Options[2].Name);
-            Assert.True(command.Options[2].IsRequired);
-            Assert.Equal("github-repos", command.Options[3].Name);
-            Assert.True(command.Options[3].IsRequired);
+            TestHelpers.VerifyCommandOption(command.Options, "github-repos", true);
         }
     }
 }
