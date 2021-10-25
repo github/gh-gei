eb249f6f417ce6951f0398e9d87726d9e0794142
Dylan Smith <dylan-smith@github.com>
2021-10-25 01:21:08 -0500

    unit tests for RewirePipelineCommand (#82)

--- src/OctoshiftCLI.Tests/Commands/RewirePipelineCommandTests.cs
+++ src/OctoshiftCLI.Tests/Commands/RewirePipelineCommandTests.cs
@@ -1,4 +1,7 @@
+using Moq;
 using OctoshiftCLI.Commands;
+using System;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace OctoshiftCLI.Tests.Commands
@@ -20,6 +23,44 @@ namespace OctoshiftCLI.Tests.Commands
             TestHelpers.VerifyCommandOption(command.Options, "github-org", true);
             TestHelpers.VerifyCommandOption(command.Options, "github-repo", true);
             TestHelpers.VerifyCommandOption(command.Options, "service-connection-id", true);
+        }
+
+        [Fact]
+        public async Task HappyPath()
+        {
+            var adoOrg = "FooOrg";
+            var adoTeamProject = "BlahTeamProject";
+            var adoPipeline = "foo-pipeline";
+            var githubOrg = "foo-gh-org";
+            var githubRepo = "foo-repo";
+            var serviceConnectionId = Guid.NewGuid().ToString();
+            var adoToken = Guid.NewGuid().ToString();
+            var pipelineId = 1234;
+            var pipeline = new AdoPipeline();
+
+            var mockAdo = new Mock<AdoApi>(string.Empty);
+            mockAdo.Setup(x => x.GetPipelineId(adoOrg, adoTeamProject, adoPipeline).Result).Returns(pipelineId);
+            mockAdo.Setup(x => x.GetPipeline(adoOrg, adoTeamProject, pipelineId).Result).Returns(pipeline);
+
+            Environment.SetEnvironmentVariable("ADO_PAT", adoToken);
+            AdoApiFactory.Create = token => token == adoToken ? mockAdo.Object : null;
+
+            var command = new RewirePipelineCommand();
+            await command.Invoke(adoOrg, adoTeamProject, adoPipeline, githubOrg, githubRepo, serviceConnectionId);
+
+            mockAdo.Verify(x => x.ChangePipelineRepo(pipeline, githubOrg, githubRepo, serviceConnectionId));
+        }
+
+        [Fact]
+        public async Task MissingADOPat()
+        {
+            // When there's no PAT it should never call the factory, forcing it to throw an exception gives us an easy way to test this
+            AdoApiFactory.Create = token => throw new InvalidOperationException();
+            Environment.SetEnvironmentVariable("ADO_PAT", string.Empty);
+
+            var command = new DisableRepoCommand();
+
+            await command.Invoke("foo", "foo", "foo");
         }
     }
 }
