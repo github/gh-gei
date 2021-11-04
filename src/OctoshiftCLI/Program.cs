ee53be763f3cd4965340730544cbad28d9c6fa56
Dylan Smith <dylan-smith@github.com>
2021-11-04 18:33:44 -0500

    got rid of CommandRegistrar class

--- src/OctoshiftCLI/Program.cs
+++ src/OctoshiftCLI/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System.CommandLine;
 using System.CommandLine.Builder;
 using System.CommandLine.Parsing;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
+using OctoshiftCLI.Commands;
 
 namespace OctoshiftCLI
 {
@@ -10,75 +12,10 @@ namespace OctoshiftCLI
     {
         public static async Task Main(string[] args)
         {
-            //var root = new RootCommand("Migrates Azure DevOps repos to GitHub");
-
-            //root.AddCommand(new GenerateScriptCommand());
-            //root.AddCommand(new RewirePipelineCommand());
-            //root.AddCommand(new IntegrateBoardsCommand());
-            //root.AddCommand(new ShareServiceConnectionCommand());
-            //root.AddCommand(new DisableRepoCommand());
-            //root.AddCommand(new LockRepoCommand());
-            //root.AddCommand(new ConfigureAutoLinkCommand());
-            //root.AddCommand(new CreateTeamCommand());
-            //root.AddCommand(new AddTeamToRepoCommand());
-            //root.AddCommand(new MigrateRepoCommand());
-            //root.AddCommand(new GrantMigratorRoleCommand());
-            //root.AddCommand(new RevokeMigratorRoleCommand());
-
             var serviceProvider = new ServiceCollection().AddCommands().BuildServiceProvider();
             var parser = BuildParser(serviceProvider);
 
             await parser.InvokeAsync(args);
-
-
-
-
-
-            //var builder = new CommandLineBuilder(root);
-            //var host = Host.CreateDefaultBuilder();
-            //host.ConfigureServices(services =>
-            //{
-            //    services.AddSingleton<AdoApi>();
-            //    services.AddSingleton(_ => AdoClientFactory.Create());
-            //    services.AddSingleton<GenerateScriptCommand>();
-            //    services.AddSingleton<RewirePipelineCommand>();
-            //    services.AddSingleton<IntegrateBoardsCommand>();
-            //    services.AddSingleton<ShareServiceConnectionCommand>();
-            //    services.AddSingleton<DisableRepoCommand>();
-            //    services.AddSingleton<LockRepoCommand>();
-            //    services.AddSingleton<ConfigureAutoLinkCommand>();
-            //    services.AddSingleton<CreateTeamCommand>();
-            //    services.AddSingleton<AddTeamToRepoCommand>();
-            //    services.AddSingleton<MigrateRepoCommand>();
-            //    services.AddSingleton<GrantMigratorRoleCommand>();
-            //    services.AddSingleton<RevokeMigratorRoleCommand>();
-            //});
-
-            //var parser = builder.UseHost(_ => Host.CreateDefaultBuilder(),
-            //                               host =>
-            //                               {
-            //                                   host.ConfigureServices(services =>
-            //                                   {
-            //                                       services.AddSingleton<AdoApi>();
-            //                                       services.AddSingleton(_ => AdoClientFactory.Create());
-            //                                       services.AddSingleton<GenerateScriptCommand>();
-            //                                       services.AddSingleton<RewirePipelineCommand>();
-            //                                       services.AddSingleton<IntegrateBoardsCommand>();
-            //                                       services.AddSingleton<ShareServiceConnectionCommand>();
-            //                                       services.AddSingleton<DisableRepoCommand>();
-            //                                       services.AddSingleton<LockRepoCommand>();
-            //                                       services.AddSingleton<ConfigureAutoLinkCommand>();
-            //                                       services.AddSingleton<CreateTeamCommand>();
-            //                                       services.AddSingleton<AddTeamToRepoCommand>();
-            //                                       services.AddSingleton<MigrateRepoCommand>();
-            //                                       services.AddSingleton<GrantMigratorRoleCommand>();
-            //                                       services.AddSingleton<RevokeMigratorRoleCommand>();
-            //                                   });
-            //                               })
-            //             .UseDefaults()
-            //             .Build();
-
-            //await parser.InvokeAsync(args);
         }
 
         private static Parser BuildParser(ServiceProvider serviceProvider)
@@ -92,6 +29,24 @@ namespace OctoshiftCLI
             }
 
             return commandLineBuilder.UseDefaults().Build();
+        }
+
+        public static IServiceCollection AddCommands(this IServiceCollection services)
+        {
+            var sampleCommandType = typeof(GenerateScriptCommand);
+            var commandType = typeof(Command);
+
+            var commands = sampleCommandType
+                .Assembly
+                .GetExportedTypes()
+                .Where(x => x.Namespace == sampleCommandType.Namespace && commandType.IsAssignableFrom(x));
+
+            foreach (var command in commands)
+            {
+                services.AddSingleton(commandType, command);
+            }
+
+            return services;
         }
     }
 }
