4f64940c1b97f5faf063c4879d1de5bc0d2234dc
Dylan Smith <dylan-smith@github.com>
2021-12-02 19:20:00 -0600

    SingleRepoTwoPipelines test

--- src/OctoshiftCLI.Tests/Commands/GenerateScriptCommandTests.cs
+++ src/OctoshiftCLI.Tests/Commands/GenerateScriptCommandTests.cs
@@ -79,6 +79,69 @@ namespace OctoshiftCLI.Tests.Commands
             Assert.Equal(expected, script);
         }
 
+        [Fact]
+        public void SingleRepoTwoPipelines()
+        {
+            var githubOrg = "foo-gh-org";
+            var adoOrg = "foo-ado-org";
+            var adoTeamProject = "foo-team-project";
+            var repo = "foo-repo";
+            var pipelineOne = "CICD";
+            var pipelineTwo = "Publish";
+            var appId = Guid.NewGuid().ToString();
+
+            var repos = new Dictionary<string, Dictionary<string, IEnumerable<string>>>
+            {
+                { adoOrg, new Dictionary<string, IEnumerable<string>>() }
+            };
+
+            repos[adoOrg].Add(adoTeamProject, new List<string>() { repo });
+
+            var pipelines = new Dictionary<string, Dictionary<string, Dictionary<string, IEnumerable<string>>>>
+            {
+                { adoOrg, new Dictionary<string, Dictionary<string, IEnumerable<string>>>() }
+            };
+
+            pipelines[adoOrg].Add(adoTeamProject, new Dictionary<string, IEnumerable<string>>());
+            pipelines[adoOrg][adoTeamProject].Add(repo, new List<string>() { pipelineOne, pipelineTwo });
+
+            var appIds = new Dictionary<string, string>
+            {
+                { adoOrg, appId }
+            };
+
+            var command = new GenerateScriptCommand(new Mock<OctoLogger>().Object, null);
+            var script = command.GenerateScript(repos, pipelines, appIds, githubOrg, false);
+
+            script = TrimNonExecutableLines(script);
+
+            var expected = $"./octoshift create-team --github-org \"{githubOrg}\" --team-name \"{adoTeamProject}-Maintainers\" --idp-group \"{adoTeamProject}-Maintainers\"";
+            expected += Environment.NewLine;
+            expected += $"./octoshift create-team --github-org \"{githubOrg}\" --team-name \"{adoTeamProject}-Admins\" --idp-group \"{adoTeamProject}-Admins\"";
+            expected += Environment.NewLine;
+            expected += $"./octoshift share-service-connection --ado-org \"{adoOrg}\" --ado-team-project \"{adoTeamProject}\" --service-connection-id \"{appId}\"";
+            expected += Environment.NewLine;
+            expected += $"./octoshift lock-ado-repo --ado-org \"{adoOrg}\" --ado-team-project \"{adoTeamProject}\" --ado-repo \"{repo}\"";
+            expected += Environment.NewLine;
+            expected += $"./octoshift migrate-repo --ado-org \"{adoOrg}\" --ado-team-project \"{adoTeamProject}\" --ado-repo \"{repo}\" --github-org \"{githubOrg}\" --github-repo \"{adoTeamProject}-{repo}\"";
+            expected += Environment.NewLine;
+            expected += $"./octoshift disable-ado-repo --ado-org \"{adoOrg}\" --ado-team-project \"{adoTeamProject}\" --ado-repo \"{repo}\"";
+            expected += Environment.NewLine;
+            expected += $"./octoshift configure-autolink --github-org \"{githubOrg}\" --github-repo \"{adoTeamProject}-{repo}\" --ado-org \"{adoOrg}\" --ado-team-project \"{adoTeamProject}\"";
+            expected += Environment.NewLine;
+            expected += $"./octoshift add-team-to-repo --github-org \"{githubOrg}\" --github-repo \"{adoTeamProject}-{repo}\" --team \"{adoTeamProject}-Maintainers\" --role \"maintain\"";
+            expected += Environment.NewLine;
+            expected += $"./octoshift add-team-to-repo --github-org \"{githubOrg}\" --github-repo \"{adoTeamProject}-{repo}\" --team \"{adoTeamProject}-Admins\" --role \"admin\"";
+            expected += Environment.NewLine;
+            expected += $"./octoshift integrate-boards --ado-org \"{adoOrg}\" --ado-team-project \"{adoTeamProject}\" --github-org \"{githubOrg}\" --github-repo \"{adoTeamProject}-{repo}\"";
+            expected += Environment.NewLine;
+            expected += $"./octoshift rewire-pipeline --ado-org \"{adoOrg}\" --ado-team-project \"{adoTeamProject}\" --ado-pipeline \"{pipelineOne}\" --github-org \"{githubOrg}\" --github-repo \"{adoTeamProject}-{repo}\" --service-connection-id \"{appId}\"";
+            expected += Environment.NewLine;
+            expected += $"./octoshift rewire-pipeline --ado-org \"{adoOrg}\" --ado-team-project \"{adoTeamProject}\" --ado-pipeline \"{pipelineTwo}\" --github-org \"{githubOrg}\" --github-repo \"{adoTeamProject}-{repo}\" --service-connection-id \"{appId}\"";
+
+            Assert.Equal(expected, script);
+        }
+
         private string TrimNonExecutableLines(string script)
         {
             var lines = script.Split(new string[] { Environment.NewLine, "\n" }, StringSplitOptions.RemoveEmptyEntries).AsEnumerable();
