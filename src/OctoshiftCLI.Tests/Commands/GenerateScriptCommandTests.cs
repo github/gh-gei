cedd1e6e9c1c07e5f6b1ee9a288c57e9ba880541
Dylan Smith <dylan-smith@github.com>
2021-12-02 19:23:58 -0600

    dotnet format

--- src/OctoshiftCLI.Tests/Commands/GenerateScriptCommandTests.cs
+++ src/OctoshiftCLI.Tests/Commands/GenerateScriptCommandTests.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using Moq;
 using OctoshiftCLI.Commands;
 using Xunit;
@@ -28,7 +26,7 @@ namespace OctoshiftCLI.Tests.Commands
         }
 
         [Fact]
-        public async Task NoData()
+        public void NoData()
         {
             var githubOrg = "foo-gh-org";
 
