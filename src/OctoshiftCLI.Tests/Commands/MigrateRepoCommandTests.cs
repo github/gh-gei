a802f5c0c2efce866cf5d1feb398ced1bacef3a7
Arin Ghazarian <aringhazarian@github.com>
2021-12-06 09:41:32 -0800

    Add githubPat to createMigrationSource mutation (#161)

--- src/OctoshiftCLI.Tests/Commands/MigrateRepoCommandTests.cs
+++ src/OctoshiftCLI.Tests/Commands/MigrateRepoCommandTests.cs
@@ -37,17 +37,20 @@ namespace OctoshiftCLI.Tests.Commands
             var githubOrgId = Guid.NewGuid().ToString();
             var migrationSourceId = Guid.NewGuid().ToString();
             var migrationId = Guid.NewGuid().ToString();
+            var githubPat = Guid.NewGuid().ToString();
 
             var mockGithub = new Mock<GithubApi>(null);
             mockGithub.Setup(x => x.GetOrganizationId(githubOrg).Result).Returns(githubOrgId);
-            mockGithub.Setup(x => x.CreateMigrationSource(githubOrgId, adoToken).Result).Returns(migrationSourceId);
+            mockGithub.Setup(x => x.CreateMigrationSource(githubOrgId, adoToken, githubPat).Result).Returns(migrationSourceId);
             mockGithub.Setup(x => x.StartMigration(migrationSourceId, adoRepoUrl, githubOrgId, githubRepo).Result).Returns(migrationId);
             mockGithub.Setup(x => x.GetMigrationState(migrationId).Result).Returns("SUCCEEDED");
 
-            using var adoFactory = new AdoApiFactory(adoToken);
-            using var githubFactory = new GithubApiFactory(mockGithub.Object);
+            var githubFactory = new Mock<GithubApiFactory>(mockGithub.Object);
+            githubFactory.Setup(m => m.GetGithubToken()).Returns(githubPat);
 
-            var command = new MigrateRepoCommand(new Mock<OctoLogger>().Object, adoFactory, githubFactory);
+            using var adoFactory = new AdoApiFactory(adoToken);
+
+            var command = new MigrateRepoCommand(new Mock<OctoLogger>().Object, adoFactory, githubFactory.Object);
             await command.Invoke(adoOrg, adoTeamProject, adoRepo, githubOrg, githubRepo);
 
             mockGithub.Verify(x => x.GetMigrationState(migrationId));
--- src/OctoshiftCLI/Commands/MigrateRepoCommand.cs
+++ src/OctoshiftCLI/Commands/MigrateRepoCommand.cs
@@ -68,9 +68,9 @@ namespace OctoshiftCLI.Commands
 
             using var github = _githubFactory.Create();
             var adoToken = _adoFactory.GetAdoToken();
-
+            var githubPat = _githubFactory.GetGithubToken();
             var githubOrgId = await github.GetOrganizationId(githubOrg);
-            var migrationSourceId = await github.CreateMigrationSource(githubOrgId, adoToken);
+            var migrationSourceId = await github.CreateMigrationSource(githubOrgId, adoToken, githubPat);
             var migrationId = await github.StartMigration(migrationSourceId, adoRepoUrl, githubOrgId, githubRepo);
 
             var migrationState = await github.GetMigrationState(migrationId);
