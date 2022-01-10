4863c40720729644d0b4bc87f803546aca4ec8b1
Dylan Smith <dylan-smith@github.com>
2022-01-10 15:05:34 -0600

    Split into ado2gh CLI and a class lib (#185)

--- publish.ps1
+++ publish.ps1
@@ -7,24 +7,24 @@ if ((Test-Path env:CLI_VERSION) -And $env:CLI_VERSION.StartsWith("refs/tags/v"))
 
 Write-Output "version: $AssemblyVersion"
 
-dotnet publish src/OctoshiftCLI/OctoshiftCLI.csproj -c Release -o dist/win-x64/ -r win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true /p:VersionPrefix=$AssemblyVersion
+dotnet publish src/ado2gh/ado2gh.csproj -c Release -o dist/win-x64/ -r win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true /p:VersionPrefix=$AssemblyVersion
 
 if ($LASTEXITCODE -ne 0) {
     exit $LASTEXITCODE
 }
 
-dotnet publish src/OctoshiftCLI/OctoshiftCLI.csproj -c Release -o dist/linux-x64/ -r linux-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true /p:VersionPrefix=$AssemblyVersion
+dotnet publish src/ado2gh/ado2gh.csproj -c Release -o dist/linux-x64/ -r linux-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true /p:VersionPrefix=$AssemblyVersion
 
 if ($LASTEXITCODE -ne 0) {
     exit $LASTEXITCODE
 }
 
-dotnet publish src/OctoshiftCLI/OctoshiftCLI.csproj -c Release -o dist/osx-x64/ -r osx-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true /p:VersionPrefix=$AssemblyVersion
+dotnet publish src/ado2gh/ado2gh.csproj -c Release -o dist/osx-x64/ -r osx-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true /p:VersionPrefix=$AssemblyVersion
 
 if ($LASTEXITCODE -ne 0) {
     exit $LASTEXITCODE
 }
 
-Compress-Archive -Path ./dist/win-x64/octoshift.exe -DestinationPath ./dist/octoshift.$AssemblyVersion.win-x64.zip -Force
-tar -cvzf ./dist/octoshift.$AssemblyVersion.linux-x64.tar.gz -C ./dist/linux-x64 octoshift
-tar -cvzf ./dist/octoshift.$AssemblyVersion.osx-x64.tar.gz -C ./dist/osx-x64 octoshift
+Compress-Archive -Path ./dist/win-x64/ado2gh.exe -DestinationPath ./dist/ado2gh.$AssemblyVersion.win-x64.zip -Force
+tar -cvzf ./dist/ado2gh.$AssemblyVersion.linux-x64.tar.gz -C ./dist/linux-x64 ado2gh
+tar -cvzf ./dist/ado2gh.$AssemblyVersion.osx-x64.tar.gz -C ./dist/osx-x64 ado2gh
