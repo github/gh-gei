b0ae9055148db8f657c6de10bad7a9efa45d8878
Dylan Smith <dylan-smith@github.com>
2022-01-12 12:01:55 -0600

    removed no-brace namespace syntax

--- src/gei/EnvironmentVariableProvider.cs
+++ src/gei/EnvironmentVariableProvider.cs
@@ -1,12 +1,14 @@
 using System;
 
-namespace OctoshiftCLI.gei;
-
-public class EnvironmentVariableProvider
+namespace OctoshiftCLI.gei
 {
-    private const string GH_PAT = "GH_PAT";
 
-    public virtual string GithubPersonalAccessToken() => Environment.GetEnvironmentVariable(GH_PAT) ??
-                                                         throw new ArgumentNullException(
-                                                             $"{GH_PAT} environment variables is not set.");
+    public class EnvironmentVariableProvider
+    {
+        private const string GH_PAT = "GH_PAT";
+
+        public virtual string GithubPersonalAccessToken() => Environment.GetEnvironmentVariable(GH_PAT) ??
+                                                             throw new ArgumentNullException(
+                                                                 $"{GH_PAT} environment variables is not set.");
+    }
 }
