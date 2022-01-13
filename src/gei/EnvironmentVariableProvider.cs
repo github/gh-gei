868c404f21678a6f0bb0c000ad936a842c208138
Dylan Smith <dylan-smith@github.com>
2022-01-12 19:56:13 -0600

    migrate repo unit test

--- src/gei/EnvironmentVariableProvider.cs
+++ src/gei/EnvironmentVariableProvider.cs
@@ -7,8 +7,22 @@ namespace OctoshiftCLI.gei
     {
         private const string GH_PAT = "GH_PAT";
 
-        public virtual string GithubPersonalAccessToken() => Environment.GetEnvironmentVariable(GH_PAT) ??
-                                                             throw new ArgumentNullException(
-                                                                 $"{GH_PAT} environment variables is not set.");
+        private readonly OctoLogger _logger;
+
+        public EnvironmentVariableProvider(OctoLogger logger)
+        {
+            _logger = logger;
+        }
+
+        public virtual string GithubPersonalAccessToken() => GetSecret(GH_PAT);
+
+        private string GetSecret(string secretName)
+        {
+            var secret = Environment.GetEnvironmentVariable(secretName) ?? throw new ArgumentNullException($"{secretName} environment variable is not set.");
+
+            _logger?.RegisterSecret(secret);
+
+            return secret;
+        }
     }
 }
