d4446f862037b715ed3a2cb188b2c354c1f1981b
Dylan Smith <dysmith@microsoft.com>
2022-01-19 13:45:57 -0800

    delete existing team projects

--- src/OctoshiftCLI.IntegrationTests/UnitTest1.cs
+++ src/OctoshiftCLI.IntegrationTests/UnitTest1.cs
@@ -1,28 +1,40 @@
 using System;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace OctoshiftCLI.IntegrationTests
 {
     public class UnitTest1
     {
+        private readonly ITestOutputHelper output;
+
+        public UnitTest1(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
         [Fact]
         public async Task Test1()
         {
-            var adoToken = Environment.GetEnvironmentVariable("ADO_PAT");
+            var adoToken = Environment.GetEnvironmentVariable("ADO_PAT"); ;
             using var httpClient = new HttpClient();
-            var adoClient = new AdoClient(null, httpClient, adoToken);
+            var adoClient = new AdoClient(new OctoLogger(), httpClient, adoToken);
             var adoApi = new AdoApi(adoClient);
 
             var adoOrg = "gei-e2e-testing";
 
             var teamProjects = await adoApi.GetTeamProjects(adoOrg);
 
+            output.WriteLine($"Found {teamProjects.Count()} Team Projects");
+
             foreach (var teamProject in teamProjects)
             {
                 if (teamProject != "service-connection-project-do-not-delete")
                 {
+                    output.WriteLine($"Deleting Team Project: {adoOrg}\\{teamProject}...");
                     var teamProjectId = await adoApi.GetTeamProjectId(adoOrg, teamProject);
                     await adoApi.DeleteTeamProject(adoOrg, teamProjectId);
                 }
