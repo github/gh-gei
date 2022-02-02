0f50d7e33690f24607c9457410d9e2c2dddf0636
Dylan Smith <dysmith@microsoft.com>
2022-02-02 09:37:50 -0600

    wip

--- src/gei/Commands/GenerateScriptCommand.cs
+++ src/gei/Commands/GenerateScriptCommand.cs
@@ -19,16 +19,21 @@ namespace OctoshiftCLI.GithubEnterpriseImporter.Commands
             _sourceGithubApiFactory = sourceGithubApiFactory;
 
             Description = "Generates a migration script. This provides you the ability to review the steps that this tool will take, and optionally modify the script if desired before running it.";
-            Description += Environment.NewLine;
-            Description += "Note: Expects GH_SOURCE_PAT or GH_PAT env variable to be set.";
 
             var githubSourceOrgOption = new Option<string>("--github-source-org")
             {
-                IsRequired = true
+                IsRequired = false,
+                Description = "Uses GH_SOURCE_PAT env variable. Will fall back to GH_PAT if not set."
+            };
+            var adoSourceOrgOption = new Option<string>("--ado-source-org")
+            {
+                IsRequired = false,
+                Description = "Uses ADO_PAT env variable."
             };
             var githubTargetOrgOption = new Option<string>("--github-target-org")
             {
-                IsRequired = true
+                IsRequired = true,
+                Description = "Uses GH_PAT env variable."
             };
             var outputOption = new Option<FileInfo>("--output", () => new FileInfo("./migrate.ps1"))
             {
@@ -44,20 +49,28 @@ namespace OctoshiftCLI.GithubEnterpriseImporter.Commands
             };
 
             AddOption(githubSourceOrgOption);
+            AddOption(adoSourceOrgOption);
             AddOption(githubTargetOrgOption);
             AddOption(outputOption);
             AddOption(ssh);
             AddOption(verbose);
 
-            Handler = CommandHandler.Create<string, string, FileInfo, bool, bool>(Invoke);
+            Handler = CommandHandler.Create<string, string, string, FileInfo, bool, bool>(Invoke);
         }
 
-        public async Task Invoke(string githubSourceOrg, string githubTargetOrg, FileInfo output, bool ssh = false, bool verbose = false)
+        public async Task Invoke(string githubSourceOrg, string adoSourceOrg, string githubTargetOrg, FileInfo output, bool ssh = false, bool verbose = false)
         {
             _log.Verbose = verbose;
 
             _log.LogInformation("Generating Script...");
-            _log.LogInformation($"GITHUB SOURCE ORG: {githubSourceOrg}");
+            if (!string.IsNullOrWhiteSpace(githubSourceOrg))
+            {
+                _log.LogInformation($"GITHUB SOURCE ORG: {githubSourceOrg}");
+            }
+            if (!string.IsNullOrWhiteSpace(adoSourceOrg))
+            {
+                _log.LogInformation($"ADO SOURCE ORG: {adoSourceOrg}");
+            }
             _log.LogInformation($"GITHUB TARGET ORG: {githubTargetOrg}");
             _log.LogInformation($"OUTPUT: {output}");
             if (ssh)
@@ -65,7 +78,19 @@ namespace OctoshiftCLI.GithubEnterpriseImporter.Commands
                 _log.LogInformation("SSH: true");
             }
 
-            var repos = await GetRepos(_sourceGithubApiFactory.Create(), githubSourceOrg);
+            if (string.IsNullOrWhiteSpace(githubSourceOrg) && string.IsNullOrWhiteSpace(adoSourceOrg))
+            {
+                throw new OctoshiftCliException("Must specify either --github-source-org or --ado-source-org");
+            }
+
+            if (string.IsNullOrWhiteSpace(githubSourceOrg))
+            {
+
+            }
+
+            var repos = string.IsNullOrWhiteSpace(githubSourceOrg) ? 
+                await GetAdoRepos(_adoApiFactory.Create(), adoSourceOrg) :
+                await GetGithubRepos(_sourceGithubApiFactory.Create(), githubSourceOrg);
 
             var script = GenerateScript(repos, githubSourceOrg, githubTargetOrg, ssh);
 
@@ -75,8 +100,51 @@ namespace OctoshiftCLI.GithubEnterpriseImporter.Commands
             }
         }
 
-        public async Task<IEnumerable<string>> GetRepos(GithubApi github, string githubOrg)
+        public async Task<IEnumerable<string>> GetGithubRepos(GithubApi github, string githubOrg)
         {
+            if (!string.IsNullOrWhiteSpace(githubOrg) && github != null)
+            {
+                _log.LogInformation($"GITHUB ORG: {githubOrg}");
+                var repos = await github.GetRepos(githubOrg);
+
+                foreach (var repo in repos)
+                {
+                    _log.LogInformation($"    Repo: {repo}");
+                }
+
+                return repos;
+            }
+
+            throw new ArgumentException("All arguments must be non-null");
+        }
+
+        public async Task<IEnumerable<string>> GetAdoRepos(AdoApi adoApi, string adoOrg)
+        {
+            var repos = new List<string>();
+
+            if (!string.IsNullOrWhiteSpace(adoOrg) && adoApi != null)
+            {
+                _log.LogInformation($"ADO ORG: {adoOrg}");
+                repos.Add(org, new Dictionary<string, IEnumerable<string>>());
+
+                var teamProjects = await ado.GetTeamProjects(org);
+
+                foreach (var teamProject in teamProjects)
+                {
+                    _log.LogInformation($"  Team Project: {teamProject}");
+                    var projectRepos = await ado.GetRepos(org, teamProject);
+                    repos[org].Add(teamProject, projectRepos);
+
+                    foreach (var repo in projectRepos)
+                    {
+                        _log.LogInformation($"    Repo: {repo}");
+                    }
+                }
+            }
+
+
+
+
             if (!string.IsNullOrWhiteSpace(githubOrg) && github != null)
             {
                 _log.LogInformation($"GITHUB ORG: {githubOrg}");
