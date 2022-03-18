18a9998b243b4bf797889f872dba2268c720c04c
Tiago Pascoal <tiago@pascoal.net>
2022-03-18 10:10:51 +0000

    Code formatting

--- src/ado2gh/Commands/GenerateScriptCommand.cs
+++ src/ado2gh/Commands/GenerateScriptCommand.cs
@@ -7,8 +7,8 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
-using OctoshiftCLI.Extensions;
 using Mono.Unix;
+using OctoshiftCLI.Extensions;
 
 namespace OctoshiftCLI.AdoToGithub.Commands
 {
