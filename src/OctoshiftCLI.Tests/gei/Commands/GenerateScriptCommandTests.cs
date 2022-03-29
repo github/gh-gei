583f7883d108e30ee6d944f2399558106ac6f0f7
Dylan Smith <dysmith@microsoft.com>
2022-03-29 16:37:20 -0500

    dotnet format

--- src/OctoshiftCLI.Tests/gei/Commands/GenerateScriptCommandTests.cs
+++ src/OctoshiftCLI.Tests/gei/Commands/GenerateScriptCommandTests.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Text;
+using System.Threading.Tasks;
 using FluentAssertions;
 using Moq;
 using OctoshiftCLI.GithubEnterpriseImporter.Commands;
