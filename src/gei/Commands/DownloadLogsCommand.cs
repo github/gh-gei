7a29b5c23c8b9c9430a08cb2f40854e7e3142992
Maxwell Pray <synthead@github.com>
2022-05-11 17:26:48 -0700

    Download migration logs with more options from DownloadLogsCommand.

--- src/gei/Commands/DownloadLogsCommand.cs
+++ src/gei/Commands/DownloadLogsCommand.cs
@@ -12,63 +12,124 @@ namespace OctoshiftCLI.GithubEnterpriseImporter.Commands
     public class DownloadLogsCommand : Command
     {
         private readonly OctoLogger _log;
+        private readonly ITargetGithubApiFactory _targetGithubApiFactory;
 
-        public DownloadLogsCommand(OctoLogger log, ITargetGithubApiFactory targetGithubApiFactory) : base("download-logs")
+        public DownloadLogsCommand(
+            OctoLogger log,
+            ITargetGithubApiFactory targetGithubApiFactory
+        ) : base("download-logs")
         {
             _log = log;
+            _targetGithubApiFactory = targetGithubApiFactory;
 
             Description = "Downloads migration logs for migrations.";
 
-            var org = new Option<string>("--github-org")
+            var githubTargetOrg = new Option<string>("--github-target-org")
             {
                 IsRequired = true,
-                Description = "GitHub organization to download migration logs for."
+                Description = "Target GitHub organization to download logs from."
             };
 
-            var repo = new Option<string>("--repo")
+            var targetRepo = new Option<string>("--target-repo")
             {
                 IsRequired = true,
-                Description = "GitHub repository to download migration log for."
+                Description = "Target repository to download latest log for."
             };
 
-            var verbose = new Option("--verbose") { IsRequired = false };
+            var targetApiUrl = new Option<string>("--target-api-url")
+            {
+                IsRequired = false,
+                Description = "Target GitHub API URL if not migrating to github.com (default: https://api.github.com)."
+            };
 
-            AddOption(org);
-            AddOption(repo);
+            var githubTargetPat = new Option<string>("--github-target-pat")
+            {
+                IsRequired = false,
+                Description = "Personal access token of the GitHub target.  Overrides GH_PAT environment variable."
+            };
+
+            var logFile = new Option<string>("--log-file")
+            {
+                IsRequired = false,
+                Description = "Local file to write log to (default: migration-log-ORG-REPO.log)."
+            };
+
+            var overwrite = new Option("--overwrite")
+            {
+                IsRequired = false,
+                Description = "Overwrite log file if it exists."
+            };
+
+            var verbose = new Option("--verbose")
+            {
+                IsRequired = false,
+                Description = "Display more information to the console."
+            };
+
+            AddOption(githubTargetOrg);
+            AddOption(targetRepo);
+            AddOption(targetApiUrl);
+            AddOption(githubTargetPat);
+            AddOption(logFile);
+            AddOption(overwrite);
             AddOption(verbose);
 
-            Handler = CommandHandler.Create<string, string, bool>(Invoke);
+            Handler = CommandHandler.Create<string, string, string, string, string, bool, bool>(Invoke);
         }
 
-        public async Task Invoke(string org, string repo, bool verbose = false)
+        public async Task Invoke(string githubTargetOrg, string targetRepo, string targetApiUrl = null, string githubTargetPat = null, string logFile = null, bool overwrite = false, bool verbose = false)
         {
             _log.Verbose = verbose;
 
-            _log.LogInformation($"Downloading logs for organization {org}...");
+            _log.LogInformation($"Downloading logs for organization {githubTargetOrg}...");
 
-            var logFile = $"migration-log-{org}-{repo}";
+            if (githubTargetPat is not null)
+            {
+                _log.LogInformation("GITHUB TARGET PAT: ***");
+            }
 
-            _log.LogInformation($"Downloading log for repository {repo} to {logFile}...");
+            logFile ??= $"migration-log-{githubTargetOrg}-{targetRepo}.log";
+
+            if (File.Exists(logFile))
+            {
+                if (!overwrite)
+                {
+                    _log.LogError($"File {logFile} already exists!  Use --overwrite to overwite this file.");
+                    return;
+                }
+
+                _log.LogWarning($"Overwriting {logFile} due to --overwrite option.");
+            }
+
+            _log.LogInformation($"Downloading log for repository {targetRepo} to {logFile}...");
+
+            var githubApi = _targetGithubApiFactory.Create(targetApiUrl, githubTargetPat);
+            var logUrl = await githubApi.GetMigrationLogUrl(githubTargetOrg, targetRepo);
+
+            if (logUrl == null)
+            {
+                _log.LogError($"Migration not found for repository {targetRepo}!");
+                return;
+            }
 
             HttpClient client = new HttpClient();
 
             try
             {
-              // FIXME use real URL here
-              using HttpResponseMessage response = await client.GetAsync("http://localhost:8080/migration-log", HttpCompletionOption.ResponseHeadersRead);
-              using Stream streamToReadFrom = await response.Content.ReadAsStreamAsync(); 
-              using Stream streamToWriteTo = File.Open(logFile, FileMode.Create); 
-              await streamToReadFrom.CopyToAsync(streamToWriteTo);
+                using HttpResponseMessage response = await client.GetAsync(logUrl, HttpCompletionOption.ResponseHeadersRead);
+                using Stream streamToReadFrom = await response.Content.ReadAsStreamAsync();
+                using Stream streamToWriteTo = File.Open(logFile, FileMode.Create);
+                await streamToReadFrom.CopyToAsync(streamToWriteTo);
 
-              _log.LogInformation($"Downloaded {repo} log to {logFile}.");
+                _log.LogInformation($"Downloaded {targetRepo} log to {logFile}.");
             }
             catch (HttpRequestException)
             {
-              _log.LogInformation($"Could not download log for repository {repo}!");
+                _log.LogInformation($"Could not download log for repository {targetRepo}!");
             }
             finally
             {
-              client.Dispose();
+                client.Dispose();
             }
         }
     }
