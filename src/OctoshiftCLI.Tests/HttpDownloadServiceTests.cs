588d55a9dc3da5f8fe4b62ec98094ac38254a21b
Maxwell Pray <synthead@github.com>
2022-05-16 12:14:01 -0700

    Use real http classes in HttpDownloadServiceTests.
    co-authored-by: Dylan Smith <dylan-smith@github.com>

--- src/OctoshiftCLI.Tests/HttpDownloadServiceTests.cs
+++ src/OctoshiftCLI.Tests/HttpDownloadServiceTests.cs
@@ -20,16 +20,30 @@ namespace OctoshiftCLI.Tests
         public async Task Downloads_File()
         {
             // Arrange
-            var url = "example-url";
+            var url = "https://objects-staging-origin.githubusercontent.com/octoshiftmigrationlogs/github/example-repo.txt";
             var filePath = "example-file";
             var fileContents = (string)null;
             var expectedFileContents = "expected-file-contents";
 
-            var mockHttpClient = TestHelpers.CreateMock<HttpClient>();
-            mockHttpClient.Setup(m => m.GetAsync(url, HttpCompletionOption.ResponseHeadersRead));
+            using var httpResponse = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(expectedFileContents)
+            };
+
+            var mockHttpHandler = new Mock<HttpMessageHandler>();
+
+            mockHttpHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.Is<HttpRequestMessage>(x => x.Method == HttpMethod.Get),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(httpResponse);
+
+            using var httpClient = new HttpClient(mockHttpHandler.Object);
 
             // Act
-            var command = new HttpDownloadService(mockHttpClient.Object) {
+            var command = new HttpDownloadService(httpClient) {
                 WriteToFile = (_, contents) =>
                 {
                     fileContents = contents;
