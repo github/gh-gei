7353942190db6fb408b2f61a1db185dbb198a19f
Maxwell Pray <synthead@github.com>
2022-05-18 13:59:33 -0700

    Add download-logs command to gei and ado2gh (#371)
    * Add DownloadLogs command to GEI.
    
    * Rename DownloadLog.cs to DownloadLogsCommand.cs.
    
    * Use DownloadLogsCommand class for download-logs command.
    
    * Use DownloadLogsCommand method in DownloadLogsCommand.
    
    * Add GetMigrationLogUrl to GithubApi.
    
    * Download migration logs with more options from DownloadLogsCommand.
    
    * Add HttpDownloadService service class.
    
    * Use HttpDownloadService service class from DownloadLogsCommand.
    
    * Check for missing migration log in DownloadLogsCommand.
    
    * Add "using Octoshift;" after System libs in DownloadLogsCommand.
    
    * refactor download logs for c#
    
    * dotnet format
    
    * added a couple unit tests for download log command
    
    * dotnet format
    
    * Break up long line in DownloadLogsCommand.
    
    * Add more happy path tests for DownloadLogsCommand.
    
    * Only test HttpDownloadService call in Happy_Path.
    
    * Add more unhappy path tests for DownloadLogsCommand.
    
    * Change param names for GithubApi.GetMigrationLogUrl.
    
    * Add tests for GithubApi.GetMigrationLogUrl.
    
    * Test for null with GetMigrationLogUrl_Returns_Null_When_No_Migration.
    
    * fixed json compare in test
    
    * Use $orgLogin GraphQL variable in GetMigrationLogUrl.
    
    * Fix GetMigrationLogUrl_Returns_Null_When_No_Migration test.
    
    * Fix Throw_OctoshiftCliException_When_No_Migration test.
    
    * Fix Throw_OctoshiftCliException_When_Migration_Log_URL_Empty test.
    
    * Make HttpDownloadService's WriteToFile readonly.
    
    * Add Downloads_File test for HttpDownloadService.
    
    * Fix test class name for HttpDownloadServiceTests.
    
    * Add InternalsVisibleTo to HttpDownloadService.
    
    * Use real http classes in HttpDownloadServiceTests.
    
    co-authored-by: Dylan Smith <dylan-smith@github.com>
    
    * Use better variable name in HttpDownloadServiceTests.
    
    co-authored-by: Dylan Smith <dylan-smith@github.com>
    
    * Test HTTP exceptions in HttpDownloadServiceTests.
    
    co-authored-by: Dylan Smith <dylan-smith@github.com>
    
    * Remove unnecessary usings from HttpDownloadServiceTests.
    
    * Remove unused usings from HttpDownloadServiceTests.
    
    * Remove using System.Linq from HttpDownloadServiceTests.
    
    * Fix curly brace style in HttpDownloadServiceTests.
    
    * Fix imports ordering in HttpDownloadService.
    
    * Add download-logs release note to RELEASENOTES.md.
    
    * Use org and repo variable names in GetMigrationLogUrl.
    
    * Consistently use four-space indents for GetMigrationLogUrl.
    
    * Remove unnecessary param to GetAsync from HttpDownloadService.
    
    * Remove retries from GetMigrationLogUrl.
    
    * Use "targeting" in --target-api-url description for DownloadLogsCommand.
    
    * Add DownloadLogsCommand to ado2gh.
    
    * Use _githubApiFactory var name in ado2gh DownloadLogsCommand.
    
    * Add DownloadLogsCommand tests.
    
    * Use better var name in ado2gh DownloadLogsCommand.
    
    * Fix command option in DownloadLogsCommandTests.
    
    * Don't use new with TestHelpers.CreateMock in DownloadLogsCommandTests.
    
    Co-authored-by: Dylan Smith <dylan-smith@github.com>

---
+++ src/gei/Commands/DownloadLogsCommand.cs
@@ -0,0 +1,149 @@
+using System;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using OctoshiftCLI.Extensions;
+
+[assembly: InternalsVisibleTo("OctoshiftCLI.Tests")]
+namespace OctoshiftCLI.GithubEnterpriseImporter.Commands
+{
+    public class DownloadLogsCommand : Command
+    {
+        private readonly OctoLogger _log;
+        private readonly ITargetGithubApiFactory _targetGithubApiFactory;
+        private readonly HttpDownloadService _httpDownloadService;
+
+        internal Func<string, bool> FileExists = path => File.Exists(path);
+
+        public DownloadLogsCommand(
+            OctoLogger log,
+            ITargetGithubApiFactory targetGithubApiFactory,
+            HttpDownloadService httpDownloadService
+        ) : base("download-logs")
+        {
+            _log = log;
+            _targetGithubApiFactory = targetGithubApiFactory;
+            _httpDownloadService = httpDownloadService;
+
+            Description = "Downloads migration logs for migrations.";
+
+            var githubTargetOrg = new Option<string>("--github-target-org")
+            {
+                IsRequired = true,
+                Description = "Target GitHub organization to download logs from."
+            };
+
+            var targetRepo = new Option<string>("--target-repo")
+            {
+                IsRequired = true,
+                Description = "Target repository to download latest log for."
+            };
+
+            var targetApiUrl = new Option<string>("--target-api-url")
+            {
+                IsRequired = false,
+                Description = "Target GitHub API URL if not targeting github.com (default: https://api.github.com)."
+            };
+
+            var githubTargetPat = new Option<string>("--github-target-pat")
+            {
+                IsRequired = false,
+                Description = "Personal access token of the GitHub target.  Overrides GH_PAT environment variable."
+            };
+
+            var migrationLogFile = new Option<string>("--migration-log-file")
+            {
+                IsRequired = false,
+                Description = "Local file to write migration log to (default: migration-log-ORG-REPO.log)."
+            };
+
+            var overwrite = new Option("--overwrite")
+            {
+                IsRequired = false,
+                Description = "Overwrite migration log file if it exists."
+            };
+
+            var verbose = new Option("--verbose")
+            {
+                IsRequired = false,
+                Description = "Display more information to the console."
+            };
+
+            AddOption(githubTargetOrg);
+            AddOption(targetRepo);
+            AddOption(targetApiUrl);
+            AddOption(githubTargetPat);
+            AddOption(migrationLogFile);
+            AddOption(overwrite);
+            AddOption(verbose);
+
+            Handler = CommandHandler.Create<string, string, string, string, string, bool, bool>(Invoke);
+        }
+
+        public async Task Invoke(
+            string githubTargetOrg,
+            string targetRepo,
+            string targetApiUrl = null,
+            string githubTargetPat = null,
+            string migrationLogFile = null,
+            bool overwrite = false,
+            bool verbose = false
+        )
+        {
+            _log.Verbose = verbose;
+
+            _log.LogInformation($"Downloading logs for organization {githubTargetOrg}...");
+
+            _log.LogInformation($"GITHUB TARGET ORG: {githubTargetOrg}");
+            _log.LogInformation($"TARGET REPO: {targetRepo}");
+
+            if (targetApiUrl.HasValue())
+            {
+                _log.LogInformation($"TARGET API URL: {targetApiUrl}");
+            }
+
+            if (githubTargetPat.HasValue())
+            {
+                _log.LogInformation("GITHUB TARGET PAT: ***");
+            }
+
+            if (migrationLogFile.HasValue())
+            {
+                _log.LogInformation($"MIGRATION LOG FILE: {migrationLogFile}");
+            }
+
+            migrationLogFile ??= $"migration-log-{githubTargetOrg}-{targetRepo}.log";
+
+            if (FileExists(migrationLogFile))
+            {
+                if (!overwrite)
+                {
+                    throw new OctoshiftCliException($"File {migrationLogFile} already exists!  Use --overwrite to overwite this file.");
+                }
+
+                _log.LogWarning($"Overwriting {migrationLogFile} due to --overwrite option.");
+            }
+
+            _log.LogInformation($"Downloading log for repository {targetRepo} to {migrationLogFile}...");
+
+            var githubApi = _targetGithubApiFactory.Create(targetApiUrl, githubTargetPat);
+            var logUrl = await githubApi.GetMigrationLogUrl(githubTargetOrg, targetRepo);
+
+            if (logUrl == null)
+            {
+                throw new OctoshiftCliException($"Migration not found for repository {targetRepo}!");
+            }
+
+            if (!logUrl.HasValue())
+            {
+                throw new OctoshiftCliException($"Migration found for repository {targetRepo}, but migration log not available yet!");
+            }
+
+            await _httpDownloadService.Download(logUrl, migrationLogFile);
+
+            _log.LogSuccess($"Downloaded {targetRepo} log to {migrationLogFile}.");
+        }
+    }
+}
