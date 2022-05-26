3306c7f202c97a57a9f65b79de3c6015f5710665
Dylan Smith <dylan-smith@github.com>
2022-05-26 12:22:55 -0500

    dotnet format

--- src/Octoshift/AdoApi.cs
+++ src/Octoshift/AdoApi.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using OctoshiftCLI.Extensions;
