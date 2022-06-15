1fad5ba22a7eb63596b9dfc415ad13a225cfe5e9
Dylan Smith <dylan-smith@github.com>
2022-06-15 11:48:44 -0500

    Add `last-push-date`, `commits-past-year`, `most-active-contributor` fields to repos.csv (#421)

--- src/ado2gh/AdoApiFactory.cs
+++ src/ado2gh/AdoApiFactory.cs
@@ -10,19 +10,21 @@ namespace OctoshiftCLI.AdoToGithub
         private readonly HttpClient _client;
         private readonly EnvironmentVariableProvider _environmentVariableProvider;
         private readonly IVersionProvider _versionProvider;
+        private readonly RetryPolicy _retryPolicy;
 
-        public AdoApiFactory(OctoLogger octoLogger, HttpClient client, EnvironmentVariableProvider environmentVariableProvider, IVersionProvider versionProvider)
+        public AdoApiFactory(OctoLogger octoLogger, HttpClient client, EnvironmentVariableProvider environmentVariableProvider, IVersionProvider versionProvider, RetryPolicy retryPolicy)
         {
             _octoLogger = octoLogger;
             _client = client;
             _environmentVariableProvider = environmentVariableProvider;
             _versionProvider = versionProvider;
+            _retryPolicy = retryPolicy;
         }
 
         public virtual AdoApi Create(string personalAccessToken)
         {
             personalAccessToken ??= _environmentVariableProvider.AdoPersonalAccessToken();
-            var adoClient = new AdoClient(_octoLogger, _client, _versionProvider, personalAccessToken);
+            var adoClient = new AdoClient(_octoLogger, _client, _versionProvider, _retryPolicy, personalAccessToken);
             return new AdoApi(adoClient, DEFAULT_API_URL, _octoLogger);
         }
     }
