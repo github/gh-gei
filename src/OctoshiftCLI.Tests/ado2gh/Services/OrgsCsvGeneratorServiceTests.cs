61b08e2c7f1bb5eb51379625649a00265fdf7655
Arin Ghazarian <aringhazarian@github.com>
2022-07-12 13:08:51 -0700

    Add minimal param to Generate methods (#480)

--- src/OctoshiftCLI.Tests/ado2gh/Services/OrgsCsvGeneratorServiceTests.cs
+++ src/OctoshiftCLI.Tests/ado2gh/Services/OrgsCsvGeneratorServiceTests.cs
@@ -10,7 +10,9 @@ namespace OctoshiftCLI.Tests.AdoToGithub.Commands
 {
     public class OrgsCsvGeneratorServiceTests
     {
-        private const string CSV_HEADER = "name,url,owner,teamproject-count,repo-count,pipeline-count,pr-count,is-pat-org-admin";
+        private const string FULL_CSV_HEADER = "name,url,owner,teamproject-count,repo-count,pipeline-count,is-pat-org-admin,pr-count";
+        private const string MINIMAL_CSV_HEADER = "name,url,owner,teamproject-count,repo-count,pipeline-count,is-pat-org-admin";
+
         private readonly Mock<AdoApi> _mockAdoApi = TestHelpers.CreateMock<AdoApi>();
         private readonly Mock<AdoApiFactory> _mockAdoApiFactory = TestHelpers.CreateMock<AdoApiFactory>();
         private readonly Mock<AdoInspectorService> _mockAdoInspectorService = TestHelpers.CreateMock<AdoInspectorService>();
@@ -52,8 +54,8 @@ namespace OctoshiftCLI.Tests.AdoToGithub.Commands
             var result = await _service.Generate(null);
 
             // Assert
-            var expected = $"{CSV_HEADER}{Environment.NewLine}";
-            expected += $"\"{ADO_ORG}\",\"https://dev.azure.com/{ADO_ORG}\",\"{owner}\",{projectCount},{repoCount},{pipelineCount},{prCount},{true}{Environment.NewLine}";
+            var expected = $"{FULL_CSV_HEADER}{Environment.NewLine}";
+            expected += $"\"{ADO_ORG}\",\"https://dev.azure.com/{ADO_ORG}\",\"{owner}\",{projectCount},{repoCount},{pipelineCount},{true},{prCount}{Environment.NewLine}";
 
             result.Should().Be(expected);
         }
@@ -68,6 +70,35 @@ namespace OctoshiftCLI.Tests.AdoToGithub.Commands
             await _service.Generate(adoPat);
 
             _mockAdoApiFactory.Verify(m => m.Create(adoPat));
+        }
+
+        [Fact]
+        public async Task Generate_Should_Return_Minimal_Csv_When_Minimal_Is_True()
+        {
+            // Arrange
+            const string owner = "Suzy (suzy@gmail.com)";
+            const int projectCount = 11;
+            const int repoCount = 82;
+            const int pipelineCount = 41;
+
+            _mockAdoApiFactory.Setup(m => m.Create(null)).Returns(_mockAdoApi.Object);
+            _mockAdoInspectorService.Setup(m => m.GetOrgs()).ReturnsAsync(_adoOrgs);
+            _mockAdoInspectorService.Setup(m => m.GetTeamProjectCount(ADO_ORG)).ReturnsAsync(projectCount);
+            _mockAdoInspectorService.Setup(m => m.GetRepoCount(ADO_ORG)).ReturnsAsync(repoCount);
+            _mockAdoInspectorService.Setup(m => m.GetPipelineCount(ADO_ORG)).ReturnsAsync(pipelineCount);
+
+            _mockAdoApi.Setup(m => m.GetOrgOwner(ADO_ORG)).ReturnsAsync(owner);
+            _mockAdoApi.Setup(m => m.IsCallerOrgAdmin(ADO_ORG)).ReturnsAsync(true);
+
+            // Act
+            var result = await _service.Generate(null, true);
+
+            // Assert
+            var expected = $"{MINIMAL_CSV_HEADER}{Environment.NewLine}";
+            expected += $"\"{ADO_ORG}\",\"https://dev.azure.com/{ADO_ORG}\",\"{owner}\",{projectCount},{repoCount},{pipelineCount},{true}{Environment.NewLine}";
+
+            result.Should().Be(expected);
+            _mockAdoInspectorService.Verify(m => m.GetPullRequestCount(It.IsAny<string>()), Times.Never);
         }
     }
 }
