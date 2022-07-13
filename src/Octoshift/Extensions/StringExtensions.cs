1827ff373942b66e48341b8be5eca7070bfd81bf
Dylan Smith <dylan-smith@github.com>
2022-07-12 22:55:44 -0500

    deleted unused extension method

--- src/Octoshift/Extensions/StringExtensions.cs
+++ src/Octoshift/Extensions/StringExtensions.cs
@@ -18,8 +18,6 @@ namespace OctoshiftCLI.Extensions
 
         public static ulong? ToULongOrNull(this string s) => ulong.TryParse(s, out var result) ? result : null;
 
-        public static IEnumerable<string> Lines(this string input) => input?.Split(new string[] { Environment.NewLine, "\n" }, StringSplitOptions.RemoveEmptyEntries);
-
         public static string ReplaceInvalidCharactersWithDash(this string s) => s.HasValue() ? Regex.Replace(s, @"[^\w.-]+", "-", RegexOptions.Compiled | RegexOptions.CultureInvariant) : string.Empty;
     }
 }
