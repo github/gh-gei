9b58fd2191c3e1cc0d95ec67b2c7d59128bf2c8b
Dylan Smith <dylanfromwinnipeg@gmail.com>
2022-08-11 19:33:17 -0500

    updated CI to publish bbs2gh

--- publish.ps1
+++ publish.ps1
@@ -118,3 +118,55 @@ else {
 
     Rename-Item ./dist/osx-x64/gei gei-darwin-amd64
 }
+
+### bbs2gh ###
+if ((Test-Path env:SKIP_WINDOWS) -And $env:SKIP_WINDOWS.ToUpper() -eq "TRUE") {
+    Write-Output "Skipping bbs2gh Windows build because SKIP_WINDOWS is set"
+}
+else {
+    dotnet publish src/bbs2gh/bbs2gh.csproj -c Release -o dist/win-x64/ -r win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true /p:VersionPrefix=$AssemblyVersion
+
+    if ($LASTEXITCODE -ne 0) {
+        exit $LASTEXITCODE
+    }
+
+    if (Test-Path -Path ./dist/win-x64/bbs2gh-windows-amd64.exe) {
+        Remove-Item ./dist/win-x64/bbs2gh-windows-amd64.exe
+    }
+
+    Rename-Item ./dist/win-x64/bbs2gh.exe bbs2gh-windows-amd64.exe
+}
+
+if ((Test-Path env:SKIP_LINUX) -And $env:SKIP_LINUX.ToUpper() -eq "TRUE") {
+    Write-Output "Skipping bbs2gh Linux build because SKIP_LINUX is set"
+}
+else {
+    dotnet publish src/bbs2gh/bbs2gh.csproj -c Release -o dist/linux-x64/ -r linux-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true /p:VersionPrefix=$AssemblyVersion
+
+    if ($LASTEXITCODE -ne 0) {
+        exit $LASTEXITCODE
+    }
+
+    if (Test-Path -Path ./dist/linux-x64/bbs2gh-linux-amd64) {
+        Remove-Item ./dist/linux-x64/bbs2gh-linux-amd64
+    }
+
+    Rename-Item ./dist/linux-x64/bbs2gh bbs2gh-linux-amd64
+}
+
+if ((Test-Path env:SKIP_MACOS) -And $env:SKIP_MACOS.ToUpper() -eq "TRUE") {
+    Write-Output "Skipping bbs2gh MacOS build because SKIP_MACOS is set"
+}
+else {
+    dotnet publish src/bbs2gh/bbs2gh.csproj -c Release -o dist/osx-x64/ -r osx-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true /p:VersionPrefix=$AssemblyVersion
+
+    if ($LASTEXITCODE -ne 0) {
+        exit $LASTEXITCODE
+    }
+
+    if (Test-Path -Path ./dist/osx-x64/bbs2gh-darwin-amd64) {
+        Remove-Item ./dist/osx-x64/bbs2gh-darwin-amd64
+    }
+
+    Rename-Item ./dist/osx-x64/bbs2gh bbs2gh-darwin-amd64
+}
