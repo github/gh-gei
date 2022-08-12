1ae1570884cb93b7832beee8e4a116ce08d8fba5
Arin Ghazarian <aringhazarian@github.com>
2022-08-12 11:31:08 -0700

    Refactor revoke-migrator-role command

--- src/ado2gh/Commands/RevokeMigratorRoleCommand.cs
+++ src/ado2gh/Commands/RevokeMigratorRoleCommand.cs
@@ -1,92 +1,19 @@
 using System;
-using System.CommandLine;
 using System.CommandLine.Invocation;
-using System.Threading.Tasks;
+using OctoshiftCLI.Commands;
+using OctoshiftCLI.Contracts;
+using OctoshiftCLI.Extensions;
 
-namespace OctoshiftCLI.AdoToGithub.Commands
+namespace OctoshiftCLI.AdoToGithub.Commands;
+
+public sealed class RevokeMigratorRoleCommand : RevokeMigratorRoleCommandBase
 {
-    public class RevokeMigratorRoleCommand : Command
+    public RevokeMigratorRoleCommand(OctoLogger log, ITargetGithubApiFactory githubApiFactory) : base(log, githubApiFactory)
     {
-        private readonly OctoLogger _log;
-        private readonly GithubApiFactory _githubApiFactory;
+        Description += Environment.NewLine;
+        Description += $"Note: Expects GH_PAT env variable or --{GithubPat.GetLogFriendlyName()} option to be set.";
 
-        public RevokeMigratorRoleCommand(OctoLogger log, GithubApiFactory githubApiFactory) : base("revoke-migrator-role")
-        {
-            _log = log;
-            _githubApiFactory = githubApiFactory;
-            Description = "Allows an organization admin to revoke the migrator role for a USER or TEAM for a single GitHub organization. This will remove their ability to run a migration into the target organization.";
-            Description += Environment.NewLine;
-            Description += "Note: Expects GH_PAT env variable or --github-pat option to be set.";
-
-            var githubOrg = new Option<string>("--github-org")
-            {
-                IsRequired = true
-            };
-            var actor = new Option<string>("--actor")
-            {
-                IsRequired = true
-            };
-            var actorType = new Option<string>("--actor-type")
-            {
-                IsRequired = true
-            };
-            var githubPat = new Option<string>("--github-pat")
-            {
-                IsRequired = false
-            };
-            var verbose = new Option("--verbose")
-            {
-                IsRequired = false
-            };
-
-            AddOption(githubOrg);
-            AddOption(actor);
-            AddOption(actorType);
-            AddOption(githubPat);
-            AddOption(verbose);
-
-            Handler = CommandHandler.Create<string, string, string, string, bool>(Invoke);
-        }
-
-        public async Task Invoke(string githubOrg, string actor, string actorType, string githubPat = null, bool verbose = false)
-        {
-            _log.Verbose = verbose;
-
-            _log.LogInformation("Granting migrator role ...");
-            _log.LogInformation($"GITHUB ORG: {githubOrg}");
-            _log.LogInformation($"ACTOR: {actor}");
-            if (githubPat is not null)
-            {
-                _log.LogInformation("GITHUB PAT: ***");
-            }
-
-            actorType = actorType?.ToUpper();
-            _log.LogInformation($"ACTOR TYPE: {actorType}");
-
-            actorType = actorType.ToUpper();
-
-            if (actorType is "TEAM" or "USER")
-            {
-                _log.LogInformation("Actor type is valid...");
-            }
-            else
-            {
-                _log.LogError("Actor type must be either TEAM or USER.");
-                return;
-            }
-
-            var githubApi = _githubApiFactory.Create(targetPersonalAccessToken: githubPat);
-            var githubOrgId = await githubApi.GetOrganizationId(githubOrg);
-            var success = await githubApi.RevokeMigratorRole(githubOrgId, actor, actorType);
-
-            if (success)
-            {
-                _log.LogSuccess($"Migrator role successfully revoked for the {actorType} \"{actor}\"");
-            }
-            else
-            {
-                _log.LogError($"Migrator role couldn't be revoked for the {actorType} \"{actor}\"");
-            }
-        }
+        AddOptions();
+        Handler = CommandHandler.Create<string, string, string, string, bool>(Handle);
     }
 }
