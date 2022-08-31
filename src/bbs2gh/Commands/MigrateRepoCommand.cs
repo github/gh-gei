eaa1f54a44c397312a1da6c6f35966957954cfd7
Dean Tambling <tambling@github.com>
2022-08-31 10:27:43 -0700

    Add archive generation to `bbs2gh migrate-repo` (#602)
    This PR adds a path to `bbs2gh migrate-repo` to generate an archive on a BBS instance if the `--bbs-server-url` argument is present. Because we're making comparatively fewer requests to a BBS instance (compared to GitHub or ADO), the logic isn't quite as complex or robust, but it does generate the archive. 
    
    Closes https://github.com/github/gh-gei/issues/524 (for real this time)
    
    ---
    
    - [x] Did you write/update appropriate tests
    - [ ] Release notes updated (if appropriate)
    - [x] Appropriate logging output
    - [ ] Issue linked
    - [ ] Docs updated (or issue created)
    
    <!--
    For docs we should review the docs at:
    https://docs.github.com/en/early-access/github/migrating-with-github-enterprise-importer
    and the README.md in this repo
    
    If a doc update is required based on the changes in this PR, it is sufficient to create an issue and link to it here. The doc update can be made later/separately.
    
    The process to update the docs can be found here:
    https://github.com/github/docs-early-access#opening-prs
    
    The markdown files are here: 
    https://github.com/github/docs-early-access/tree/main/content/github/migrating-with-github-enterprise-importer
    -->
    
    Co-authored-by: Arin Ghazarian <aringhazarian@github.com>
    Co-authored-by: Dylan Smith <dylan-smith@github.com>

--- src/bbs2gh/Commands/MigrateRepoCommand.cs
+++ src/bbs2gh/Commands/MigrateRepoCommand.cs
@@ -2,141 +2,253 @@
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.Threading.Tasks;
+using OctoshiftCLI.Extensions;
 
-namespace OctoshiftCLI.BbsToGithub.Commands
+namespace OctoshiftCLI.BbsToGithub.Commands;
+
+public class MigrateRepoCommand : Command
 {
-    public class MigrateRepoCommand : Command
+    private readonly OctoLogger _log;
+    private readonly GithubApiFactory _githubApiFactory;
+    private readonly BbsApiFactory _bbsApiFactory;
+    private readonly EnvironmentVariableProvider _environmentVariableProvider;
+    private const int CHECK_STATUS_DELAY_IN_MILLISECONDS = 10000;
+
+    public MigrateRepoCommand(
+        OctoLogger log,
+        GithubApiFactory githubApiFactory,
+        BbsApiFactory bbsApiFactory,
+        EnvironmentVariableProvider environmentVariableProvider
+    ) : base("migrate-repo")
     {
-        private readonly OctoLogger _log;
-        private readonly GithubApiFactory _githubApiFactory;
-        private readonly EnvironmentVariableProvider _environmentVariableProvider;
+        _log = log;
+        _githubApiFactory = githubApiFactory;
+        _bbsApiFactory = bbsApiFactory;
+        _environmentVariableProvider = environmentVariableProvider;
 
-        public MigrateRepoCommand(
-            OctoLogger log,
-            GithubApiFactory githubApiFactory,
-            EnvironmentVariableProvider environmentVariableProvider
-        ) : base("migrate-repo")
+        Description = "Import a Bitbucket Server archive to GitHub.";
+        Description += Environment.NewLine;
+        Description += "Note: Expects GH_PAT env variable or --github-pat option to be set.";
+
+        // Arguments to generate a new archive
+        var bbsServerUrl = new Option<string>("--bbs-server-url")
         {
-            _log = log;
-            _githubApiFactory = githubApiFactory;
-            _environmentVariableProvider = environmentVariableProvider;
+            IsRequired = false,
+            Description = "The full URL of the Bitbucket Server/Data Center to migrate from. E.g. http://bitbucket.contoso.com:7990"
+        };
 
-            Description = "Import a Bitbucket Server archive to GitHub.";
-            Description += Environment.NewLine;
-            Description += "Note: Expects GH_PAT env variable or --github-pat option to be set.";
+        var bbsProject = new Option<string>("--bbs-project")
+        {
+            IsRequired = false,
+            Description = "The Bitbucket project to migrate."
+        };
 
-            var archiveUrl = new Option<string>("--archive-url")
-            {
-                IsRequired = true,
-                Description = "URL used to download Bitbucket Server migration archive."
-            };
-            var githubOrg = new Option<string>("--github-org")
-            {
-                IsRequired = true
-            };
-            var githubRepo = new Option<string>("--github-repo")
-            {
-                IsRequired = true
-            };
-            var githubPat = new Option<string>("--github-pat")
-            {
-                IsRequired = false
-            };
-            var wait = new Option("--wait")
-            {
-                Description = "Synchronously waits for the repo migration to finish."
-            };
-            var verbose = new Option("--verbose")
-            {
-                IsRequired = false
-            };
+        var bbsRepo = new Option<string>("--bbs-repo")
+        {
+            IsRequired = false,
+            Description = "The Bitbucket repository to migrate."
+        };
 
-            AddOption(archiveUrl);
-            AddOption(githubOrg);
-            AddOption(githubRepo);
-            AddOption(githubPat);
-            AddOption(wait);
-            AddOption(verbose);
+        var bbsUsername = new Option<string>("--bbs-username")
+        {
+            IsRequired = false,
+            Description = "The Bitbucket username of a user with site admin privileges. If not set will be read from BBS_USERNAME environment variable."
+        };
 
-            Handler = CommandHandler.Create<MigrateRepoCommandArgs>(Invoke);
+        var bbsPassword = new Option<string>("--bbs-password")
+        {
+            IsRequired = false,
+            Description = "The Bitbucket password of the user specified by --bbs-username. If not set will be read from BBS_PASSWORD environment variable."
+        };
+
+        // Arguments to import an existing archive
+        var archiveUrl = new Option<string>("--archive-url")
+        {
+            IsRequired = false,
+            Description = "URL used to download Bitbucket Server migration archive. Only needed if you want to manually retrieve the archive from BBS instead of letting this CLI do that for you."
+        };
+        var githubOrg = new Option<string>("--github-org")
+        {
+            IsRequired = false
+        };
+        var githubRepo = new Option<string>("--github-repo")
+        {
+            IsRequired = false
+        };
+        var githubPat = new Option<string>("--github-pat")
+        {
+            IsRequired = false
+        };
+        var wait = new Option("--wait")
+        {
+            Description = "Synchronously waits for the repo migration to finish."
+        };
+        var verbose = new Option("--verbose")
+        {
+            IsRequired = false
+        };
+
+        AddOption(archiveUrl);
+        AddOption(githubOrg);
+        AddOption(githubRepo);
+        AddOption(githubPat);
+
+        AddOption(bbsServerUrl);
+        AddOption(bbsProject);
+        AddOption(bbsRepo);
+        AddOption(bbsUsername);
+        AddOption(bbsPassword);
+
+        AddOption(wait);
+        AddOption(verbose);
+
+        Handler = CommandHandler.Create<MigrateRepoCommandArgs>(Invoke);
+    }
+
+    public async Task Invoke(MigrateRepoCommandArgs args)
+    {
+        if (args is null)
+        {
+            throw new ArgumentNullException(nameof(args));
         }
 
-        public async Task Invoke(MigrateRepoCommandArgs args)
+        if (!args.BbsServerUrl.HasValue() && !args.ArchiveUrl.HasValue())
         {
-            if (args is null)
-            {
-                throw new ArgumentNullException(nameof(args));
-            }
+            throw new OctoshiftCliException("Either --bbs-server-url or --archive-url must be specified.");
+        }
 
-            _log.Verbose = args.Verbose;
+        if (args.BbsServerUrl.HasValue() && args.ArchiveUrl.HasValue())
+        {
+            throw new OctoshiftCliException("Only one of --bbs-server-url or --archive-url can be specified.");
+        }
 
-            _log.LogInformation("Migrating Repo...");
-            _log.LogInformation($"GITHUB ORG: {args.GithubOrg}");
-            _log.LogInformation($"GITHUB REPO: {args.GithubRepo}");
+        _log.Verbose = args.Verbose;
 
-            if (args.GithubPat is not null)
-            {
-                _log.LogInformation("GITHUB PAT: ***");
-            }
-            if (args.Wait)
-            {
-                _log.LogInformation("WAIT: true");
-            }
-
-            args.GithubPat ??= _environmentVariableProvider.GithubPersonalAccessToken();
-            var githubApi = _githubApiFactory.Create(targetPersonalAccessToken: args.GithubPat);
-            var githubOrgId = await githubApi.GetOrganizationId(args.GithubOrg);
-            var migrationSourceId = await githubApi.CreateBbsMigrationSource(githubOrgId);
-
-            string migrationId;
-
-            try
-            {
-                migrationId = await githubApi.StartBbsMigration(migrationSourceId, githubOrgId, args.GithubRepo, args.GithubPat, args.ArchiveUrl);
-            }
-            catch (OctoshiftCliException ex)
-            {
-                if (ex.Message == $"A repository called {args.GithubOrg}/{args.GithubRepo} already exists")
-                {
-                    _log.LogWarning($"The Org '{args.GithubOrg}' already contains a repository with the name '{args.GithubRepo}'. No operation will be performed");
-                    return;
-                }
-
-                throw;
-            }
-
-            if (!args.Wait)
-            {
-                _log.LogInformation($"A repository migration (ID: {migrationId}) was successfully queued.");
-                return;
-            }
-
-            var (migrationState, _, failureReason) = await githubApi.GetMigration(migrationId);
-
-            while (RepositoryMigrationStatus.IsPending(migrationState))
-            {
-                _log.LogInformation($"Migration in progress (ID: {migrationId}). State: {migrationState}. Waiting 10 seconds...");
-                await Task.Delay(10000);
-                (migrationState, _, failureReason) = await githubApi.GetMigration(migrationId);
-            }
-
-            if (RepositoryMigrationStatus.IsFailed(migrationState))
-            {
-                _log.LogError($"Migration Failed. Migration ID: {migrationId}");
-                throw new OctoshiftCliException(failureReason);
-            }
-
-            _log.LogSuccess($"Migration completed (ID: {migrationId})! State: {migrationState}");
+        if (args.BbsServerUrl.HasValue())
+        {
+            await GenerateArchive(args);
+        }
+        else if (args.ArchiveUrl.HasValue())
+        {
+            await ImportArchive(args);
         }
     }
 
-    public class MigrateRepoCommandArgs
+    private async Task GenerateArchive(MigrateRepoCommandArgs args)
     {
-        public string ArchiveUrl { get; set; }
-        public string GithubOrg { get; set; }
-        public string GithubRepo { get; set; }
-        public string GithubPat { get; set; }
-        public bool Wait { get; set; }
-        public bool Verbose { get; set; }
+        args.BbsUsername ??= _environmentVariableProvider.BbsUsername();
+        args.BbsPassword ??= _environmentVariableProvider.BbsPassword();
+
+        if (!args.BbsUsername.HasValue())
+        {
+            throw new OctoshiftCliException("BBS username must be either set as BBS_USERNAME environment variable or passed as --bbs-username.");
+        }
+
+        if (!args.BbsPassword.HasValue())
+        {
+            throw new OctoshiftCliException("BBS password must be either set as BBS_PASSWORD environment variable or passed as --bbs-password.");
+        }
+
+        var bbsApi = _bbsApiFactory.Create(args.BbsServerUrl, args.BbsUsername, args.BbsPassword);
+
+        _log.LogInformation($"BBS SERVER URL: {args.BbsServerUrl}...");
+        _log.LogInformation($"BBS PROJECT: {args.BbsProject}");
+        _log.LogInformation($"BBS REPO: {args.BbsRepo}");
+
+        var exportId = await bbsApi.StartExport(args.BbsProject, args.BbsRepo);
+
+        if (!args.Wait)
+        {
+            _log.LogInformation($"Export started. Export ID: {exportId}");
+            return;
+        }
+
+        var (exportState, exportMessage, exportProgress) = await bbsApi.GetExport(exportId);
+
+        while (ExportState.IsInProgress(exportState))
+        {
+            _log.LogInformation($"Export status: {exportState}; {exportProgress}% complete");
+            await Task.Delay(CHECK_STATUS_DELAY_IN_MILLISECONDS);
+            (exportState, exportMessage, exportProgress) = await bbsApi.GetExport(exportId);
+        }
+
+        if (ExportState.IsError(exportState))
+        {
+            throw new OctoshiftCliException($"Bitbucket export failed --> State: {exportState}; Message: {exportMessage}");
+        }
+
+        _log.LogInformation($"Export completed. Your migration archive should be ready on your instance at $BITBUCKET_SHARED_HOME/data/migration/export/Bitbucket_export_{exportId}.tar");
     }
+
+    private async Task ImportArchive(MigrateRepoCommandArgs args)
+    {
+        _log.LogInformation("Migrating Repo...");
+        _log.LogInformation($"GITHUB ORG: {args.GithubOrg}");
+        _log.LogInformation($"GITHUB REPO: {args.GithubRepo}");
+
+        if (args.GithubPat is not null)
+        {
+            _log.LogInformation("GITHUB PAT: ***");
+        }
+        if (args.Wait)
+        {
+            _log.LogInformation("WAIT: true");
+        }
+
+        args.GithubPat ??= _environmentVariableProvider.GithubPersonalAccessToken();
+        var githubApi = _githubApiFactory.Create(targetPersonalAccessToken: args.GithubPat);
+        var githubOrgId = await githubApi.GetOrganizationId(args.GithubOrg);
+        var migrationSourceId = await githubApi.CreateBbsMigrationSource(githubOrgId);
+
+        string migrationId;
+
+        try
+        {
+            migrationId = await githubApi.StartBbsMigration(migrationSourceId, githubOrgId, args.GithubRepo, args.GithubPat, args.ArchiveUrl);
+        }
+        catch (OctoshiftCliException ex) when (ex.Message == $"A repository called {args.GithubOrg}/{args.GithubRepo} already exists")
+        {
+            _log.LogWarning($"The Org '{args.GithubOrg}' already contains a repository with the name '{args.GithubRepo}'. No operation will be performed");
+            return;
+        }
+
+        if (!args.Wait)
+        {
+            _log.LogInformation($"A repository migration (ID: {migrationId}) was successfully queued.");
+            return;
+        }
+
+        var (migrationState, _, failureReason) = await githubApi.GetMigration(migrationId);
+
+        while (RepositoryMigrationStatus.IsPending(migrationState))
+        {
+            _log.LogInformation($"Migration in progress (ID: {migrationId}). State: {migrationState}. Waiting 10 seconds...");
+            await Task.Delay(CHECK_STATUS_DELAY_IN_MILLISECONDS);
+            (migrationState, _, failureReason) = await githubApi.GetMigration(migrationId);
+        }
+
+        if (RepositoryMigrationStatus.IsFailed(migrationState))
+        {
+            throw new OctoshiftCliException($"Migration #{migrationId} failed: {failureReason}");
+        }
+
+        _log.LogSuccess($"Migration completed (ID: {migrationId})! State: {migrationState}");
+
+    }
+}
+
+public class MigrateRepoCommandArgs
+{
+    public string ArchiveUrl { get; set; }
+    public string GithubOrg { get; set; }
+    public string GithubRepo { get; set; }
+    public string GithubPat { get; set; }
+    public bool Wait { get; set; }
+    public bool Verbose { get; set; }
+
+    public string BbsServerUrl { get; set; }
+    public string BbsProject { get; set; }
+    public string BbsRepo { get; set; }
+    public string BbsUsername { get; set; }
+    public string BbsPassword { get; set; }
 }
