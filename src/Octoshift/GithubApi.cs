13d6d1bdd691efc824a55618a9f64d5a6cc14587
Peter Murray <681306+peter-murray@users.noreply.github.com>
2022-09-07 16:19:47 +0100

    Update src/Octoshift/GithubApi.cs

--- src/Octoshift/GithubApi.cs
+++ src/Octoshift/GithubApi.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using Octoshift.Models;
