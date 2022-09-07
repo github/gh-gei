ed88cf7bccfc1b3b278ee813bdc94778a0e27c6c
Arin Ghazarian <aringhazarian@github.com>
2022-09-06 20:50:28 -0700

    Generate script works

--- src/bbs2gh/Commands/GenerateScriptCommand.cs
+++ src/bbs2gh/Commands/GenerateScriptCommand.cs
@@ -2,6 +2,7 @@ using System;
 using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 using OctoshiftCLI.Contracts;
 using OctoshiftCLI.Extensions;
@@ -22,15 +23,42 @@ public class GenerateScriptCommand : Command
 
         Description = "Generates a migration script. This provides you the ability to review the steps that this tool will take, and optionally modify the script if desired before running it.";
 
-        var bbsServerUrl = new Option<string>("--bbs-server-url") { IsRequired = true, Description = "The full URL of the Bitbucket Server/Data Center to migrate from." };
+        var bbsServerUrl = new Option<string>("--bbs-server-url")
+        {
+            IsRequired = true,
+            Description = "The full URL of the Bitbucket Server/Data Center to migrate from."
+        };
         var githubOrg = new Option<string>("--github-org") { IsRequired = true };
-        var sequential = new Option<bool>("--sequential") { IsRequired = false, Description = "Waits for each migration to finish before moving on to the next one." };
+        var bbsUsername = new Option<string>("--bbs-username")
+        {
+            IsRequired = false,
+            Description = "The Bitbucket username of a user with site admin privileges. If not set will be read from BBS_USERNAME environment variable."
+        };
+        var sshUser = new Option<string>("--ssh-user")
+        {
+            IsRequired = false,
+            Description = "The SSH user to be used for downloading the export archive off of the Bitbucket server."
+        };
+        var sshPrivateKey = new Option<string>("--ssh-private-key")
+        {
+            IsRequired = false,
+            Description = "The full path of the private key file to be used for downloading the export archive off of the Bitbucket Server using SSH/SFTP."
+        };
+        var sshPort = new Option<int>("--ssh-port")
+        {
+            IsRequired = false,
+            Description = "The SSH port (default: 22)."
+        };
+
         var output = new Option<FileInfo>("--output", () => new FileInfo("./migrate.ps1")) { IsRequired = false };
         var verbose = new Option<bool>("--verbose") { IsRequired = false };
 
         AddOption(bbsServerUrl);
         AddOption(githubOrg);
-        AddOption(sequential);
+        AddOption(bbsUsername);
+        AddOption(sshUser);
+        AddOption(sshPrivateKey);
+        AddOption(sshPort);
         AddOption(output);
         AddOption(verbose);
 
@@ -50,13 +78,44 @@ public class GenerateScriptCommand : Command
 
         LogOptions(args);
 
-        var script = "this is a sample script";
+        var script = GenerateScript(args);
 
         if (script.HasValue() && args.Output.HasValue())
         {
             await _fileSystemProvider.WriteAllTextAsync(args.Output.FullName, script);
         }
     }
+
+    private string GenerateScript(GenerateScriptCommandArgs args)
+    {
+        var content = new StringBuilder();
+
+        content.AppendLine(PWSH_SHEBANG);
+        content.AppendLine();
+        content.AppendLine(VersionComment);
+        content.AppendLine(EXEC_FUNCTION_BLOCK);
+
+        content.AppendLine($"# =========== Organization: {args.GithubOrg} ===========");
+
+        content.AppendLine(Exec(MigrateGithubRepoScript(args, true)));
+
+        return content.ToString();
+    }
+
+    private string MigrateGithubRepoScript(GenerateScriptCommandArgs args, bool wait)
+    {
+        var bbsUsername = args.BbsUsername.HasValue() ? $" --bbs-username \"{args.BbsUsername}\"" : "";
+
+        var archiveDownloadOptions = args.SshUser.HasValue()
+            ? $" --ssh-user \"{args.SshUser}\" --ssh-private-key \"{args.SshPrivateKey}\"{(args.SshPort.HasValue() ? $" --ssh-port {args.SshPort}" : "")}"
+            : "";
+
+        return $"gh bbs2gh migrate-repo --github-org \"{args.GithubOrg}\" --bbs-server-url \"{args.BbsServerUrl}\"{bbsUsername}{archiveDownloadOptions}{(wait ? " --wait" : "")}";
+    }
+
+    private string Exec(string script) => Wrap(script, "Exec");
+
+    private string Wrap(string script, string outerCommand = "") => script.IsNullOrWhiteSpace() ? string.Empty : $"{outerCommand} {{ {script} }}".Trim();
 
     private void LogOptions(GenerateScriptCommandArgs args)
     {
@@ -70,23 +129,51 @@ public class GenerateScriptCommand : Command
             _log.LogInformation($"GITHUB ORG: {args.GithubOrg}");
         }
 
+        if (args.SshUser.HasValue())
+        {
+            _log.LogInformation($"SSH USER: {args.SshUser}");
+        }
+
+        if (args.SshPrivateKey.HasValue())
+        {
+            _log.LogInformation($"SSH PRIVATE KEY: {args.SshPrivateKey}");
+        }
+
+        if (args.SshPort.HasValue())
+        {
+            _log.LogInformation($"SSH PORT: {args.SshPort}");
+        }
+
         if (args.Output.HasValue())
         {
             _log.LogInformation($"OUTPUT: {args.Output}");
         }
-
-        if (args.Sequential.HasValue())
-        {
-            _log.LogInformation($"SEQUENTIAL: {args.Sequential}");
-        }
     }
+
+    private string VersionComment => $"# =========== Created with CLI version {_versionProvider.GetCurrentVersion()} ===========";
+
+    private const string PWSH_SHEBANG = "#!/usr/bin/env pwsh";
+
+    private const string EXEC_FUNCTION_BLOCK = @"
+function Exec {
+    param (
+        [scriptblock]$ScriptBlock
+    )
+    & @ScriptBlock
+    if ($lastexitcode -ne 0) {
+        exit $lastexitcode
+    }
+}";
 }
 
 public class GenerateScriptCommandArgs
 {
     public string BbsServerUrl { get; set; }
     public string GithubOrg { get; set; }
-    public bool Sequential { get; set; }
+    public string BbsUsername { get; set; }
+    public string SshUser { get; set; }
+    public string SshPrivateKey { get; set; }
+    public string SshPort { get; set; }
     public FileInfo Output { get; set; }
     public bool Verbose { get; set; }
 }
