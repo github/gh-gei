484ea82d92e9ec70af9f1573fc715cc65275a637
Maxwell Pray <synthead@github.com>
2022-09-08 18:08:14 -0700

    Add RetryPolicy param to GithubClient in integration tests.

--- src/OctoshiftCLI.IntegrationTests/GhesToGithub.cs
+++ src/OctoshiftCLI.IntegrationTests/GhesToGithub.cs
@@ -47,11 +47,11 @@ public sealed class GhesToGithub : IDisposable
         _versionClient = new HttpClient();
 
         _sourceGithubHttpClient = new HttpClient();
-        _sourceGithubClient = new GithubClient(logger, _sourceGithubHttpClient, new VersionChecker(_versionClient, logger), sourceGithubToken);
+        _sourceGithubClient = new GithubClient(logger, _sourceGithubHttpClient, new VersionChecker(_versionClient, logger), new RetryPolicy(logger), sourceGithubToken);
         _sourceGithubApi = new GithubApi(_sourceGithubClient, GHES_API_URL, new RetryPolicy(logger));
 
         _targetGithubHttpClient = new HttpClient();
-        _targetGithubClient = new GithubClient(logger, _targetGithubHttpClient, new VersionChecker(_versionClient, logger), targetGithubToken);
+        _targetGithubClient = new GithubClient(logger, _targetGithubHttpClient, new VersionChecker(_versionClient, logger), new RetryPolicy(logger), targetGithubToken);
         _targetGithubApi = new GithubApi(_targetGithubClient, "https://api.github.com", new RetryPolicy(logger));
 
         _blobServiceClient = new BlobServiceClient(azureStorageConnectionString);
