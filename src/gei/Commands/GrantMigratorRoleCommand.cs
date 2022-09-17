dab3d5eb1d022cbbdda9d2cfec350e9d9ba0d60d
Dylan Smith <dylan-smith@github.com>
2022-09-17 00:15:09 -0500

    GrantMigratorRoleCommandArgs

--- src/gei/Commands/GrantMigratorRoleCommand.cs
+++ src/gei/Commands/GrantMigratorRoleCommand.cs
@@ -15,11 +15,11 @@ public sealed class GrantMigratorRoleCommand : GrantMigratorRoleCommandBase
         Description += $"Note: Expects GH_PAT env variable or --{GithubPat.ArgumentHelpName} option to be set.";
 
         AddOptions();
-        Handler = CommandHandler.Create<string, string, string, string, bool>(Invoke);
+        Handler = CommandHandler.Create<GrantMigratorRoleCommandArgs>(Invoke);
     }
 
     protected override Option<string> GithubPat { get; } = new("--github-target-pat") { IsRequired = false };
 
-    public async Task Invoke(string githubOrg, string actor, string actorType, string githubTargetPat = null, bool verbose = false) =>
-        await Handle(githubOrg, actor, actorType, githubTargetPat, verbose);
+    public async Task Invoke(GrantMigratorRoleCommandArgs args) =>
+        await Handle(args);
 }
