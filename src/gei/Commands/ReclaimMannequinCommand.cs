6efeeaab65a4943f0fd68a82d7b1ec76a1ffe994
Dylan Smith <dylan-smith@github.com>
2022-09-17 00:25:19 -0500

    ReclaimMannequinCommandArgs

--- src/gei/Commands/ReclaimMannequinCommand.cs
+++ src/gei/Commands/ReclaimMannequinCommand.cs
@@ -12,7 +12,7 @@ public sealed class ReclaimMannequinCommand : ReclaimMannequinCommandBase
     public ReclaimMannequinCommand(OctoLogger log, ITargetGithubApiFactory targetGithubApiFactory, ReclaimService reclaimService = null) : base(log, targetGithubApiFactory, reclaimService)
     {
         AddOptions();
-        Handler = CommandHandler.Create<string, string, string, string, string, bool, string, bool>(Invoke);
+        Handler = CommandHandler.Create<ReclaimMannequinCommandArgs>(Invoke);
     }
 
     protected override Option<string> GithubOrg { get; } = new("--github-target-org")
@@ -26,13 +26,5 @@ public sealed class ReclaimMannequinCommand : ReclaimMannequinCommandBase
         IsRequired = false
     };
 
-    public async Task Invoke(
-        string githubTargetOrg,
-        string csv,
-        string mannequinUser,
-        string mannequinId,
-        string targetUser,
-        bool force = false,
-        string githubTargetPat = null,
-        bool verbose = false) => await Handle(githubTargetOrg, csv, mannequinUser, mannequinId, targetUser, force, githubTargetPat, verbose);
+    public async Task Invoke(ReclaimMannequinCommandArgs args) => await Handle(args);
 }
