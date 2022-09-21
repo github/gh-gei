e24251028eefc504cff9e6f3540dcf800e13f336
Maxwell Pray <synthead@github.com>
2022-09-20 21:02:01 -0500

    Add OctoshiftCLI.BbsToGithub.AwsApiFactory.

---
+++ src/bbs2gh/AwsApiFactory.cs
@@ -0,0 +1,22 @@
+namespace OctoshiftCLI.BbsToGithub
+{
+    public sealed class AwsApiFactory : IAwsApiFactory
+    {
+        private readonly EnvironmentVariableProvider _environmentVariableProvider;
+        private readonly OctoLogger _octoLogger;
+
+        public AwsApiFactory(EnvironmentVariableProvider environmentVariableProvider, OctoLogger octoLogger)
+        {
+            _environmentVariableProvider = environmentVariableProvider;
+            _octoLogger = octoLogger;
+        }
+
+        public AwsApi Create(string awsAccessKey = null, string awsSecretKey = null)
+        {
+            var accessKey = string.IsNullOrWhiteSpace(awsAccessKey) ? _environmentVariableProvider.AwsAccessKey() : awsAccessKey;
+            var secretKey = string.IsNullOrWhiteSpace(awsSecretKey) ? _environmentVariableProvider.AwsSecretKey() : awsSecretKey;
+
+            return new AwsApi(accessKey, secretKey, _octoLogger);
+        }
+    }
+}
