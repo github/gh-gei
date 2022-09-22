a7fda683a4cb8d3f69d099f4d42d171ee7f14dec
Maxwell Pray <synthead@github.com>
2022-09-22 03:00:35 -0500

    Remove GetBucketRegion from OctoshiftCLI.AwsApi.

--- src/Octoshift/AwsApi.cs
+++ src/Octoshift/AwsApi.cs
@@ -13,6 +13,8 @@ namespace OctoshiftCLI
         private readonly string _awsSecretKey;
         private readonly OctoLogger _log;
 
+        private static readonly RegionEndpoint regionEndpoint = RegionEndpoint.USEast1;
+
         private const int AUTHORIZATION_TIMEOUT_IN_HOURS = 24;
 
         public AwsApi(string awsAccessKey, string awsSecretKey, OctoLogger log)
@@ -24,24 +26,12 @@ namespace OctoshiftCLI
 
         public virtual async Task<Uri> UploadToBucket(string bucketName, string fileName, string keyName)
         {
-            var awsCredentials = new Amazon.Runtime.BasicAWSCredentials(_awsAccessKey, _awsSecretKey);
-            var bucketRegion = await GetBucketRegion(bucketName);
-            using var amazonS3Client = new AmazonS3Client(awsCredentials, bucketRegion);
+            using var amazonS3Client = new AmazonS3Client(_awsAccessKey, _awsSecretKey, regionEndpoint);
             using var transferUtility = new TransferUtility(amazonS3Client);
 
             await transferUtility.UploadAsync(fileName, bucketName, keyName);
 
             return GetPreSignedUrlForFile(amazonS3Client, bucketName, keyName);
-        }
-
-        public virtual async Task<RegionEndpoint> GetBucketRegion(string bucketName)
-        {
-            // FIXME GetBucketLocation is inaccessible due to its protection level?
-            // using var amazonS3Client = new AmazonS3Client(_awsAccessKey, _awsSecretKey);
-            // return await amazonS3Client.GetBucketLocation(bucketName);
-            //
-            // FIXME detect region from bucket name
-            return await Task.Run(() => RegionEndpoint.USEast1);
         }
 
         private Uri GetPreSignedUrlForFile(AmazonS3Client amazonS3Client, string bucketName, string keyName)
