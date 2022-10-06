473a829a178cbe6e79f6d2f057c579d41370bca3
Dean Tambling <tambling@github.com>
2022-10-06 18:16:17 +0000

    Add and polish some tests

--- src/OctoshiftCLI.Tests/AwsApiTests.cs
+++ src/OctoshiftCLI.Tests/AwsApiTests.cs
@@ -1,3 +1,5 @@
+using System.IO;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Amazon.S3;
@@ -13,7 +15,7 @@ namespace OctoshiftCLI.Tests;
 public class AwsApiTests
 {
     [Fact]
-    public async Task UploadToBucket_Should_Succeed()
+    public async Task UploadFileToBucket_Should_Succeed()
     {
         // Arrange
         var bucketName = "bucket";
@@ -26,22 +28,35 @@ public class AwsApiTests
 
         s3Client.Setup(m => m.GetPreSignedURL(It.IsAny<GetPreSignedUrlRequest>())).Returns(url);
         transferUtility.Setup(m => m.S3Client).Returns(s3Client.Object);
-        var awsApi = new AwsApi(transferUtility.Object);
+        using var awsApi = new AwsApi(transferUtility.Object);
 
-        // Act
-        string result;
-
-        try
-        {
-            result = await awsApi.UploadFileToBucket(bucketName, fileName, keyName);
-        }
-        finally
-        {
-            awsApi.Dispose();
-        }
+        var result = await awsApi.UploadFileToBucket(bucketName, fileName, keyName);
 
         // Assert
         result.Should().Be(url);
         transferUtility.Verify(m => m.UploadAsync(fileName, bucketName, keyName, It.IsAny<CancellationToken>()));
+    }
+
+    [Fact]
+    public async Task UploadBytesToBucket_Should_Succeed()
+    {
+        // Arrange
+        var bucketName = "bucket";
+        var bytes = Encoding.ASCII.GetBytes("here are some bytes");
+        var keyName = "key";
+        var url = "http://example.com/file.zip";
+
+        var transferUtility = new Mock<ITransferUtility>();
+        var s3Client = new Mock<IAmazonS3>();
+
+        s3Client.Setup(m => m.GetPreSignedURL(It.IsAny<GetPreSignedUrlRequest>())).Returns(url);
+        transferUtility.Setup(m => m.S3Client).Returns(s3Client.Object);
+        using var awsApi = new AwsApi(transferUtility.Object);
+
+        var result = await awsApi.UploadBytesToBucket(bucketName, bytes, keyName);
+
+        // Assert
+        result.Should().Be(url);
+        transferUtility.Verify(m => m.UploadAsync(It.IsAny<MemoryStream>(), bucketName, keyName, It.IsAny<CancellationToken>()));
     }
 }
