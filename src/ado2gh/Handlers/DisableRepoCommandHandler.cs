059e91f61011e9695a2cb957c3b165ad52e41d4d
Dylan Smith <dylan-smith@github.com>
2022-10-11 22:14:56 -0500

    DisableRepoCommand

--- src/ado2gh/Handlers/DisableRepoCommandHandler.cs
+++ src/ado2gh/Handlers/DisableRepoCommandHandler.cs
@@ -2,21 +2,22 @@
 using System.Linq;
 using System.Threading.Tasks;
 using OctoshiftCLI.AdoToGithub.Commands;
+using OctoshiftCLI.Handlers;
 
 namespace OctoshiftCLI.AdoToGithub.Handlers;
 
-public class DisableRepoCommandHandler
+public class DisableRepoCommandHandler : ICommandHandler<DisableRepoCommandArgs>
 {
     private readonly OctoLogger _log;
-    private readonly AdoApiFactory _adoApiFactory;
+    private readonly AdoApi _adoApi;
 
-    public DisableRepoCommandHandler(OctoLogger log, AdoApiFactory adoApiFactory)
+    public DisableRepoCommandHandler(OctoLogger log, AdoApi adoApi)
     {
         _log = log;
-        _adoApiFactory = adoApiFactory;
+        _adoApi = adoApi;
     }
 
-    public async Task Invoke(DisableRepoCommandArgs args)
+    public async Task Handle(DisableRepoCommandArgs args)
     {
         if (args is null)
         {
@@ -36,16 +37,14 @@ public class DisableRepoCommandHandler
 
         _log.RegisterSecret(args.AdoPat);
 
-        var ado = _adoApiFactory.Create(args.AdoPat);
-
-        var allRepos = await ado.GetRepos(args.AdoOrg, args.AdoTeamProject);
+        var allRepos = await _adoApi.GetRepos(args.AdoOrg, args.AdoTeamProject);
         if (allRepos.Any(r => r.Name == args.AdoRepo && r.IsDisabled))
         {
             _log.LogSuccess($"Repo '{args.AdoOrg}/{args.AdoTeamProject}/{args.AdoRepo}' is already disabled - No action will be performed");
             return;
         }
         var repoId = allRepos.First(r => r.Name == args.AdoRepo).Id;
-        await ado.DisableRepo(args.AdoOrg, args.AdoTeamProject, repoId);
+        await _adoApi.DisableRepo(args.AdoOrg, args.AdoTeamProject, repoId);
 
         _log.LogSuccess("Repo successfully disabled");
     }
