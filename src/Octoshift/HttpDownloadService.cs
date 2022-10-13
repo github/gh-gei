f2b580b03aebc7a2aae56a037a92d18deff679a1
Dean Tambling <tambling@github.com>
2022-10-13 05:36:21 +0000

    Add timeout to HttpDownloadService client

--- src/Octoshift/HttpDownloadService.cs
+++ src/Octoshift/HttpDownloadService.cs
@@ -18,6 +18,11 @@ namespace OctoshiftCLI
         {
             _log = log;
             _httpClient = httpClient;
+
+            if (_httpClient is not null)
+            {
+                _httpClient.Timeout = TimeSpan.FromHours(1);
+            }
         }
 
         public virtual async Task DownloadToFile(string url, string file)
