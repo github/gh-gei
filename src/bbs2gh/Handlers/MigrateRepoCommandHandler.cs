0b4033daef7ea821b1fe85a2363a9ec84199396b
Dylan Smith <dylan-smith@github.com>
2022-11-05 01:34:42 -0500

    reduced cyclomatic complexity

--- src/bbs2gh/Handlers/MigrateRepoCommandHandler.cs
+++ src/bbs2gh/Handlers/MigrateRepoCommandHandler.cs
@@ -342,18 +342,6 @@ public class MigrateRepoCommandHandler : ICommandHandler<MigrateRepoCommandArgs>
             throw new OctoshiftCliException("Only one of --archive-path or --archive-url can be specified.");
         }
 
-        if ((args.SshUser.HasValue() && args.SshPrivateKey.IsNullOrWhiteSpace()) ||
-            (args.SshPrivateKey.HasValue() && args.SshUser.IsNullOrWhiteSpace()))
-        {
-            throw new OctoshiftCliException("Both --ssh-user and --ssh-private-key must be specified for SSH download.");
-        }
-
-        if ((args.SmbUser.HasValue() && args.SmbPassword.IsNullOrWhiteSpace()) ||
-            (args.SmbPassword.HasValue() && args.SmbUser.IsNullOrWhiteSpace()))
-        {
-            throw new OctoshiftCliException("Both --smb-user and --smb-password must be specified for SMB download.");
-        }
-
         if (ShouldGenerateArchive(args))
         {
             if (GetBbsUsername(args).IsNullOrWhiteSpace())
@@ -400,6 +388,18 @@ public class MigrateRepoCommandHandler : ICommandHandler<MigrateRepoCommandArgs>
         if (shouldUseSsh && shouldUseSmb)
         {
             throw new OctoshiftCliException("You can't provide both SSH and SMB credentials together.");
+        }
+
+        if ((args.SshUser.HasValue() && args.SshPrivateKey.IsNullOrWhiteSpace()) ||
+            (args.SshPrivateKey.HasValue() && args.SshUser.IsNullOrWhiteSpace()))
+        {
+            throw new OctoshiftCliException("Both --ssh-user and --ssh-private-key must be specified for SSH download.");
+        }
+
+        if ((args.SmbUser.HasValue() && args.SmbPassword.IsNullOrWhiteSpace()) ||
+            (args.SmbPassword.HasValue() && args.SmbUser.IsNullOrWhiteSpace()))
+        {
+            throw new OctoshiftCliException("Both --smb-user and --smb-password must be specified for SMB download.");
         }
     }
 
