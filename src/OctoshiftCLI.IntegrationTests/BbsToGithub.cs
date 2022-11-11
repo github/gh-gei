a575d50d61dee65120f25f0a14d41e1c26311a6b
Dylan Smith <dysmith@microsoft.com>
2022-11-10 22:20:29 -0500

    updated it to only run against 8.5.0

--- src/OctoshiftCLI.IntegrationTests/BbsToGithub.cs
+++ src/OctoshiftCLI.IntegrationTests/BbsToGithub.cs
@@ -65,8 +65,7 @@ public sealed class BbsToGithub : IDisposable
     }
 
     [Theory]
-    //[InlineData("http://e2e-bbs-8-5-0-linux-2204.eastus.cloudapp.azure.com:7990")]
-    [InlineData("http://e2e-bbs-5-14-0-linux-2004.eastus.cloudapp.azure.com:7990")]
+    [InlineData("http://e2e-bbs-8-5-0-linux-2204.eastus.cloudapp.azure.com:7990")]
     public async Task Basic(string bbsServer)
     {
         var bbsProjectKey = $"E2E-{TestHelper.GetOsName().ToUpper()}";
