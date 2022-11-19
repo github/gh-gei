57ee1dba964caf1734c4ad0641e474b800608258
Dylan Smith <dysmith@microsoft.com>
2022-11-19 13:53:35 -0500

    messed up merge of bbs2gh migrate-repo logic

--- src/bbs2gh/Commands/migrate-repo/MigrateRepoCommandHandler.cs
+++ src/bbs2gh/Commands/migrate-repo/MigrateRepoCommandHandler.cs
@@ -71,7 +71,7 @@ public class MigrateRepoCommandHandler : ICommandHandler<MigrateRepoCommandArgs>
         if (ShouldUploadArchive(args))
         {
             // This is for the case where the CLI is being run on the BBS server itself
-            if (args.ArchivePath.IsNullOrWhiteSpace() && args.ArchiveUrl.IsNullOrWhiteSpace())
+            if (args.ArchivePath.IsNullOrWhiteSpace())
             {
                 args.ArchivePath = _bbsArchiveDownloader.GetSourceExportArchiveAbsolutePath(exportId);
             }
@@ -389,14 +389,12 @@ public class MigrateRepoCommandHandler : ICommandHandler<MigrateRepoCommandArgs>
             throw new OctoshiftCliException("You can't provide both SSH and SMB credentials together.");
         }
 
-        if ((args.SshUser.HasValue() && args.SshPrivateKey.IsNullOrWhiteSpace()) ||
-            (args.SshPrivateKey.HasValue() && args.SshUser.IsNullOrWhiteSpace()))
+        if (args.SshUser.HasValue() ^ args.SshPrivateKey.HasValue())
         {
             throw new OctoshiftCliException("Both --ssh-user and --ssh-private-key must be specified for SSH download.");
         }
 
-        if ((args.SmbUser.HasValue() && args.SmbPassword.IsNullOrWhiteSpace()) ||
-            (args.SmbPassword.HasValue() && args.SmbUser.IsNullOrWhiteSpace()))
+        if (args.SmbUser.HasValue() ^ args.SmbPassword.HasValue())
         {
             throw new OctoshiftCliException("Both --smb-user and --smb-password must be specified for SMB download.");
         }
