e96007c97a1a46e5bc473f738b6508bc934a86d4
David Losert <davelosert@github.com>
2022-11-23 15:17:35 +0100

    Fix unit tests by switching to Pattern

--- src/OctoshiftCLI.Tests/StringCompressorTests.cs
+++ src/OctoshiftCLI.Tests/StringCompressorTests.cs
@@ -9,10 +9,12 @@ public class StringCompressorTests
     public void GZipAndBase64String_Correctly_Compresses_String()
     {
         var uncompressed = "uncompressed_test_string";
-        var expectedString = "H4sIAAAAAAAAEyvNS87PLShKLS5OTYkvSS0uiS8uKcrMSwcAdqGS8xgAAAA=";
+        // It seems like, depending on the underlying OS, two characters in the Zipped and Base64 encoded string differ.
+        // So we simply check for all variants here to cover Linux, Mac OSX and Windows.
+        var expectedStringPattern = "H4sIAAAAAAAA(Ey|Ci|Ay)vNS87PLShKLS5OTYkvSS0uiS8uKcrMSwcAdqGS8xgAAAA=";
 
         var actualString = StringCompressor.GZipAndBase64String(uncompressed);
-
-        Assert.Equal(expectedString, actualString);
+        
+        Assert.Matches("H4sIAAAAAAAA(Ey|Ci|Ay)vNS87PLShKLS5OTYkvSS0uiS8uKcrMSwcAdqGS8xgAAAA=",actualString);
     }
 }
