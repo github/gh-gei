a3a2cfa1098c987843441d5bdf526e01293dcfb9
Tim Rogers <me@timrogers.co.uk>
2022-12-13 09:38:56 +0000

    Tweak error message for organization lookup failure

--- src/Octoshift/GithubApi.cs
+++ src/Octoshift/GithubApi.cs
@@ -148,7 +148,7 @@ namespace OctoshiftCLI
             });
 
             return response.Outcome == OutcomeType.Failure
-                ? throw new OctoshiftCliException($"Failed to lookup the Organization ID for {org}", response.FinalException)
+                ? throw new OctoshiftCliException($"Failed to lookup the Organization ID for organization '{org}'", response.FinalException)
                 : response.Result;
         }
 
