0290472838d8e0ad609436ae288551c0abdabd55
hfishback01 <hfishback01@github.com>
2023-03-20 09:34:57 -0700

    fix test

--- src/OctoshiftCLI.Tests/Octoshift/HttpDownloadServiceFactoryTests.cs
+++ src/OctoshiftCLI.Tests/Octoshift/HttpDownloadServiceFactoryTests.cs
@@ -77,24 +77,6 @@ namespace OctoshiftCLI.Tests.GithubEnterpriseImporter.Commands
             // Assert
             httpDownloadService.Should().NotBeNull();
         }
-
-        [Fact]
-        public void Creates_HttpDownloadService_Allowing_Redirect()
-        {
-            // Arrange
-            using var httpClient = new HttpClient();
-
-            _mockHttpClientFactory
-                .Setup(x => x.CreateClient("NoSSL"))
-                .Returns(httpClient);
-
-
-            // Act
-            var httpDownloadService = _httpDownloadServiceFactory.CreateWithRedirectAllowed();
-
-            // Assert
-            httpDownloadService.Should().NotBeNull();
-        }
     }
 }
 
