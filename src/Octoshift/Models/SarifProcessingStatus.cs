f2367cef0fbc8cf7975693fe93fa67cf9046f957
Dylan Smith <dylanfromwinnipeg@gmail.com>
2023-03-23 18:31:47 +0000

    fix dotnet format error

--- src/Octoshift/Models/SarifProcessingStatus.cs
+++ src/Octoshift/Models/SarifProcessingStatus.cs
@@ -9,7 +9,7 @@ public class SarifProcessingStatus
     public const string Pending = "pending";
 
     public string Status { get; set; }
-    public Collection<string> Errors { get; set; }
+    public Collection<string> Errors { get; init; }
 
     public static bool IsPending(string status) => status?.Trim().ToLower() is Pending;
     public static bool IsFailed(string status) => status?.Trim().ToLower() is Failed;
