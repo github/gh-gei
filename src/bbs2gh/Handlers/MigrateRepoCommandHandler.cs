88c9a2e5d6b58896fa0d0c0509e9bcf09a143487
Dylan Smith <dylanfromwinnipeg@gmail.com>
2023-04-06 18:33:19 -0500

    unit tests for ado and bb2

--- src/bbs2gh/Handlers/MigrateRepoCommandHandler.cs
+++ src/bbs2gh/Handlers/MigrateRepoCommandHandler.cs
@@ -229,7 +229,7 @@ public class MigrateRepoCommandHandler : ICommandHandler<MigrateRepoCommandArgs>
             return;
         }
 
-        if (!args.Wait)
+        if (args.QueueOnly)
         {
             _log.LogInformation($"A repository migration (ID: {migrationId}) was successfully queued.");
             return;
@@ -476,7 +476,7 @@ public class MigrateRepoCommandHandler : ICommandHandler<MigrateRepoCommandArgs>
 
         if (!args.Wait && !args.QueueOnly)
         {
-            _log.LogWarning("The default behavior has changed from only queueing the migration, to waiting for the migraiton to finish. If you ran this as part of a script to run multiple migrations in parallel, consider using the new --queue-only option to preserve the previous default behavior.");
+            _log.LogWarning("The default behavior has changed from only queueing the migration, to waiting for the migration to finish. If you ran this as part of a script to run multiple migrations in parallel, consider using the new --queue-only option to preserve the previous default behavior.");
         }
     }
 
