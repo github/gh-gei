61f71ad0368609cd24c033e2495659aa8f16d9a4
Junko Suzuki <pnsk@github.com>
2023-04-26 10:31:25 +0900

    Reorder the SUCCESS log level

--- src/Octoshift/Services/OctoLogger.cs
+++ src/Octoshift/Services/OctoLogger.cs
@@ -11,8 +11,8 @@ internal static class LogLevel
     public const string INFO = "INFO";
     public const string WARNING = "WARNING";
     public const string ERROR = "ERROR";
-    public const string VERBOSE = "DEBUG";
     public const string SUCCESS = "INFO";
+    public const string VERBOSE = "DEBUG";
 }
 
 public class OctoLogger
