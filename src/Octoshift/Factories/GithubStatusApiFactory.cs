3a762e8613af4cf3b601a8719546531848e33dc9
Maxwell Pray <synthead@github.com>
2023-05-23 16:08:28 -0700

    Add GithubStatusApi, GithubStatusClient, and GithubStatusApiFactory.
    co-authored-by: Ana Stankovic <mulana@github.com>

---
+++ src/Octoshift/Factories/GithubStatusApiFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using OctoshiftCLI.Contracts;
+using OctoshiftCLI.Services;
+
+namespace OctoshiftCLI.Factories;
+
+public class GithubStatusApiFactory
+{
+    private const string GITHUB_STATUS_API_URL = "https://www.githubstatus.com/api/v2";
+
+    private readonly OctoLogger _octoLogger;
+    private readonly HttpClient _client;
+    private readonly IVersionProvider _versionProvider;
+
+    public GithubStatusApiFactory(OctoLogger octoLogger, HttpClient client, IVersionProvider versionProvider)
+    {
+        _octoLogger = octoLogger;
+        _client = client;
+        _versionProvider = versionProvider;
+    }
+
+    public virtual GithubStatusApi Create()
+    {
+        var githubStatusClient = new GithubStatusClient(_octoLogger, _client, _versionProvider);
+        return new GithubStatusApi(githubStatusClient, GITHUB_STATUS_API_URL);
+    }
+}
---
+++ src/Octoshift/Services/GithubStatusApi.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace OctoshiftCLI.Services;
+
+public class GithubStatusApi
+{
+    private readonly GithubStatusClient _client;
+    private readonly string _baseUrl;
+
+    public GithubStatusApi(GithubStatusClient client, string baseUrl)
+    {
+        _client = client;
+        _baseUrl = baseUrl;
+    }
+
+    public virtual async Task<int> GetUnresolvedIncidentsCount()
+    {
+        var url = $"{_baseUrl}/incidents/unresolved.json";
+        var response = await _client.GetAsync(url);
+
+        return JObject.Parse(response)["incidents"].Count();
+    }
+}
