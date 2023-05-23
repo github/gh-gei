2f3e1980acde6e27c1a38db2479603b81f1fd1f5
Tim Rogers <timrogers@github.com>
2023-05-23 14:41:00 +0100

    Improve description

--- src/Octoshift/Commands/ReclaimMannequin/ReclaimMannequinCommandBase.cs
+++ src/Octoshift/Commands/ReclaimMannequin/ReclaimMannequinCommandBase.cs
@@ -60,7 +60,7 @@ public class ReclaimMannequinCommandBase : CommandBase<ReclaimMannequinCommandAr
     public virtual Option<bool> SkipInvitation { get; } = new("--skip-invitation")
     {
         IsHidden = true,
-        Description = "Reclaim mannequins immediately without sending an invitation to the user. Warning: this is irreversible! This feature is currently in private beta, and is only supported for selected Enterprise Managed Users (EMU) organizations."
+        Description = "Reclaim mannequins immediately without sending an invitation to the user. Warning: this is irreversible! This feature is currently in development, and is not ready for sure. When it launches, it will only be supported for selected Enterprise Managed Users (EMU) organizations."
     };
 
     public virtual Option<bool> Verbose { get; } = new("--verbose");
