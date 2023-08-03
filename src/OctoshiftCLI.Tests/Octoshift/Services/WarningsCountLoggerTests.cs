c20a45758cb8fc41338124d65a7d5f82e02bfd4d
gohana <32278992+gohana@users.noreply.github.com>
2023-08-03 16:36:10 -0700

    Test WarningsCountLogger service

---
+++ src/OctoshiftCLI.Tests/Octoshift/Services/WarningsCountLoggerTests.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using FluentAssertions;
+using Moq;
+using OctoshiftCLI.Services;
+using Xunit;
+
+namespace OctoshiftCLI.Tests.Octoshift.Services;
+
+public class WarningsCountLoggerTests
+{
+  private string _logOutput;
+  private string _verboseLogOutput;
+  private string _consoleOutput;
+  private string _consoleError;
+
+  private readonly OctoLogger _octoLogger;
+  private readonly WarningsCountLogger _warningsCountLogger;
+
+  public WarningsCountLoggerTests()
+  {
+    _octoLogger = new OctoLogger(CaptureLogOutput, CaptureVerboseLogOutput, CaptureConsoleOutput, CaptureConsoleError);
+    _warningsCountLogger = new WarningsCountLogger(_octoLogger);
+  }
+
+  [Fact]
+  public void LogWarningsCount_Should_Write_No_Warnings_To_Console_Out()
+  {
+    _warningsCountLogger.LogWarningsCount(0);
+
+    _consoleOutput.Should().Contain("No warnings encountered during this migration");
+  }
+
+  [Fact]
+  public void LogWarningsCount_Should_Write_1_Warning_To_Console_Out()
+  {
+    _warningsCountLogger.LogWarningsCount(1);
+
+    _consoleOutput.Should().Contain("1 warning encountered during this migration");
+  }
+
+  [Fact]
+  public void LogWarningsCount_Should_Write_Warnings_Count_To_Console_Out()
+  {
+    _warningsCountLogger.LogWarningsCount(3);
+
+    _consoleOutput.Should().Contain($"3 warnings encountered during this migration");
+  }
+
+  private void CaptureLogOutput(string msg) => _logOutput += msg;
+
+  private void CaptureVerboseLogOutput(string msg) => _verboseLogOutput += msg;
+
+  private void CaptureConsoleOutput(string msg) => _consoleOutput += msg;
+
+  private void CaptureConsoleError(string msg) => _consoleError += msg;
+}
