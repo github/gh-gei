f64e75811c525b80ae8a45be9ce0de83ab4e5544
Tim Rogers <me@timrogers.co.uk>
2023-10-23 20:47:17 +0100

    Tweak logs outputted during latest version check

--- src/bbs2gh/Program.cs
+++ src/bbs2gh/Program.cs
@@ -117,11 +117,11 @@ namespace OctoshiftCLI.BbsToGithub
 
             if (await versionChecker.IsLatest())
             {
-                Logger.LogInformation($"You are running the latest version of the bbs2gh extension [v{await versionChecker.GetLatestVersion()}]");
+                Logger.LogInformation($"You are running an up-to-date version of the bbs2gh extension [v{versionChecker.GetCurrentVersion()}]");
             }
             else
             {
-                Logger.LogWarning($"You are running an older version of the bbs2gh extension [v{versionChecker.GetCurrentVersion()}]. The latest version is v{await versionChecker.GetLatestVersion()}.");
+                Logger.LogWarning($"You are running an old version of the bbs2gh extension [v{versionChecker.GetCurrentVersion()}]. The latest version is v{await versionChecker.GetLatestVersion()}.");
                 Logger.LogWarning($"Please update by running: gh extension upgrade bbs2gh");
             }
         }
