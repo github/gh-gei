a9a7bcf2d30b27f431b3a1fcca462b5ca9e9ab5d
Begona Guereca <begonaguereca@github.com>
2023-12-15 10:50:56 -0800

    Update src/bbs2gh/Commands/AbortMigration/WaitForMigrationCommand.cs
    Co-authored-by: Arin Ghazarian <aringhazarian@github.com>

--- src/bbs2gh/Commands/AbortMigration/WaitForMigrationCommand.cs
+++ src/bbs2gh/Commands/AbortMigration/WaitForMigrationCommand.cs
@@ -1,6 +1,6 @@
 using OctoshiftCLI.Commands.AbortMigration;
 
-namespace OctoshiftCLI.BbsToGithub.Commands.WaitForMigration;
+namespace OctoshiftCLI.BbsToGithub.Commands.AbortMigration;
 
 public sealed class AbortMigrationCommand : AbortMigrationCommandBase
 {
