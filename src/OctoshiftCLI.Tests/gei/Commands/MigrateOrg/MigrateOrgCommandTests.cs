833931b72b39a5e1dd253973774f603db509a397
Briana J <brianaj@github.com>
2024-01-30 19:44:08 +0000

    add migrate org test

--- src/OctoshiftCLI.Tests/gei/Commands/MigrateOrg/MigrateOrgCommandTests.cs
+++ src/OctoshiftCLI.Tests/gei/Commands/MigrateOrg/MigrateOrgCommandTests.cs
@@ -1,11 +1,35 @@
 using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using OctoshiftCLI.GithubEnterpriseImporter.Commands.MigrateOrg;
+using OctoshiftCLI.Contracts;
+using OctoshiftCLI.Services;
 using Xunit;
 
 namespace OctoshiftCLI.Tests.GithubEnterpriseImporter.Commands.MigrateOrg
 {
     public class MigrateOrgCommandTests
     {
+        private readonly Mock<ITargetGithubApiFactory> _mockGithubApiFactory = new();
+        private readonly Mock<OctoLogger> _mockOctoLogger = TestHelpers.CreateMock<OctoLogger>();
+        private readonly Mock<EnvironmentVariableProvider> _mockEnvironmentVariableProvider = TestHelpers.CreateMock<EnvironmentVariableProvider>();
+        private readonly Mock<WarningsCountLogger> _warningsCountLogger = TestHelpers.CreateMock<WarningsCountLogger>();
+
+        private readonly ServiceProvider _serviceProvider;
+        private readonly MigrateOrgCommand _command = new();
+
+        public MigrateOrgCommandTests()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection
+                .AddSingleton(_mockOctoLogger.Object)
+                .AddSingleton(_mockGithubApiFactory.Object)
+                .AddSingleton(_mockEnvironmentVariableProvider.Object)
+                .AddSingleton(_warningsCountLogger.Object);
+
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+        }
+
         [Fact]
         public void Should_Have_Options()
         {
@@ -22,6 +46,28 @@ namespace OctoshiftCLI.Tests.GithubEnterpriseImporter.Commands.MigrateOrg
             TestHelpers.VerifyCommandOption(command.Options, "github-target-pat", false);
             TestHelpers.VerifyCommandOption(command.Options, "verbose", false);
             TestHelpers.VerifyCommandOption(command.Options, "target-api-url", false);
+        }
+
+        [Fact]
+        public void It_Uses_Target_Api_Url_When_Provided()
+        {
+            var githubSourcePat = "abc123";
+            var githubTargetPat = "def456";
+            var targetApiUrl = "https://api.github.com";
+
+            var args = new MigrateOrgCommandArgs
+            {
+                GithubSourceOrg = "source-org",
+                GithubSourcePat = githubSourcePat,
+                GithubTargetOrg = "target-org",
+                GithubTargetEnterprise = "target-enterprise",
+                GithubTargetPat = githubTargetPat,
+                TargetApiUrl = targetApiUrl
+            };
+
+            _command.BuildHandler(args, _serviceProvider);
+
+            _mockGithubApiFactory.Verify(m => m.Create(targetApiUrl, githubTargetPat));
         }
     }
 }
