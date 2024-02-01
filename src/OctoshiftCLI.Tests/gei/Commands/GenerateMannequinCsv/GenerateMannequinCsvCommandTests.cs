ced01f24b414691818b67c6283c1ecbd680671c2
Briana J <brianaj@github.com>
2024-02-01 17:44:27 +0000

    fix GenerateMannequinCsvCommandTests

--- src/OctoshiftCLI.Tests/gei/Commands/GenerateMannequinCsv/GenerateMannequinCsvCommandTests.cs
+++ src/OctoshiftCLI.Tests/gei/Commands/GenerateMannequinCsv/GenerateMannequinCsvCommandTests.cs
@@ -11,12 +11,13 @@ public class GenerateMannequinCsvCommandTests
         var command = new GenerateMannequinCsvCommand();
         Assert.NotNull(command);
         Assert.Equal("generate-mannequin-csv", command.Name);
-        Assert.Equal(5, command.Options.Count);
+        Assert.Equal(6, command.Options.Count);
 
         TestHelpers.VerifyCommandOption(command.Options, "github-target-org", true);
         TestHelpers.VerifyCommandOption(command.Options, "output", false);
         TestHelpers.VerifyCommandOption(command.Options, "include-reclaimed", false);
         TestHelpers.VerifyCommandOption(command.Options, "github-target-pat", false);
         TestHelpers.VerifyCommandOption(command.Options, "verbose", false);
+        TestHelpers.VerifyCommandOption(command.Options, "target-api-url", false);
     }
 }
