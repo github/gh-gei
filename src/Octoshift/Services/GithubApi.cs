032f3b0e4f048464805f8dab8ad203a1fa9feed2
Begona Guereca <begonaguereca@github.com>
2024-10-01 09:51:11 -0700

    resolve build errors

--- src/Octoshift/Services/GithubApi.cs
+++ src/Octoshift/Services/GithubApi.cs
@@ -1082,36 +1082,6 @@ public class GithubApi
         }
     }
 
-<<<<<<< Updated upstream
-=======
-    public virtual async Task<string> UploadArchiveToGithubStorage(string org, bool isMultipart, string archiveName, Stream archiveContent)
-    {
-        using var httpContent = new StreamContent(archiveContent);
-        string response;
-
-        if (isMultipart)
-        {
-            var url = $"https://uploads.github.com/organizations/{org.EscapeDataString()}/gei/archive/blobs/uploads";
-
-            using var content = new MultipartFormDataContent
-            {
-                { httpContent, "archive", archiveName }
-            };
-
-            response = await _client.PostAsync(url, content);
-        }
-        else
-        {
-            var url = $"https://uploads.github.com/organizations/93741352/gei/archive?name={archiveName}";
-            // DEV: var url = $"http://uploads.github.localhost/organizations/{org.EscapeDataString()}/gei/archive?name={archiveName}"
-            response = await _client.PostAsync(url, httpContent);
-        }
-
-        var data = JObject.Parse(response);
-        return "gei://archive/" + (string)data["archiveId"];
-    }
-
->>>>>>> Stashed changes
     private static object GetMannequinsPayload(string orgId)
     {
         var query = "query($id: ID!, $first: Int, $after: String)";
