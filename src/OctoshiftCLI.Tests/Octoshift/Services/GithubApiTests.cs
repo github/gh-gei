60e9ed1d26de72088f60e7d8b771fdb90d75ca1b
Begona Guereca <begonaguereca@github.com>
2024-10-08 10:22:39 -0700

    alphabetical order

--- src/OctoshiftCLI.Tests/Octoshift/Services/GithubApiTests.cs
+++ src/OctoshiftCLI.Tests/Octoshift/Services/GithubApiTests.cs
@@ -1,7 +1,7 @@
 using System;
-using System.IO;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
