ca6c476350b9b65395c560a2f9c7827e74810e53
Begona Guereca <begonaguereca@github.com>
2024-10-09 14:38:57 -0700

    fix linter errors

--- src/OctoshiftCLI.Tests/Octoshift/Services/GithubClientTests.cs
+++ src/OctoshiftCLI.Tests/Octoshift/Services/GithubClientTests.cs
@@ -1,10 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.IO;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
