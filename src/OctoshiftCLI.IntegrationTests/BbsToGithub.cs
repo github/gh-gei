739368c89aed1e688c133129f4f7a30658286e05
bbsadmin <begonaguereca@github.com>
2024-11-18 12:53:29 -0800

    Try it by making it a generate script

--- src/OctoshiftCLI.IntegrationTests/BbsToGithub.cs
+++ src/OctoshiftCLI.IntegrationTests/BbsToGithub.cs
@@ -142,7 +142,7 @@ public sealed class BbsToGithub : IDisposable
     }
 
     [Fact]
-    public async Task MigrateRepo_MultipartUpload()
+    public async Task StartingFromScractch()
     {
         var githubTargetOrg = $"octoshift-e2e-bbs-{TestHelper.GetOsName()}";
         var bbsProjectKey = $"IN";
@@ -161,15 +161,46 @@ public sealed class BbsToGithub : IDisposable
            await _targetHelper.ResetGithubTestEnvironment(githubTargetOrg);
        });
 
-        var migrateRepoCommand = $"migrate-repo --bbs-server-url {bbsServer} --bbs-project {bbsProjectKey} --bbs-repo {bbsRepo} --github-org {githubTargetOrg} --github-repo{targetRepo} --ssh-user octoshift --ssh-private-key {SSH_KEY_FILE} --use-github-storage --verbose";
-
-        await _targetHelper.RunBbsMigrateRepoCommand(migrateRepoCommand, _tokens);
+        await _targetHelper.RunBbsCliMigration(
+     $"generate-script --github-org {githubTargetOrg} --bbs-server-url {bbsServer} --bbs-project {bbsProjectKey} --ssh-user octoshift --ssh-private-key {SSH_KEY_FILE} --use-github-storage", _tokens);
 
         _targetHelper.AssertNoErrorInLogs(_startTime);
 
-        await _targetHelper.AssertGithubRepoExists(githubTargetOrg, targetRepo);
-        await _targetHelper.AssertGithubRepoInitialized(githubTargetOrg, targetRepo);
+        await _targetHelper.AssertGithubRepoExists(githubTargetOrg, targetRepo1);
+        await _targetHelper.AssertGithubRepoExists(githubTargetOrg, targetRepo2);
+        await _targetHelper.AssertGithubRepoInitialized(githubTargetOrg, targetRepo1);
+        await _targetHelper.AssertGithubRepoInitialized(githubTargetOrg, targetRepo2);
     }
+
+    // [Fact]
+    // public async Task MigrateRepo_MultipartUpload()
+    // {
+    //     var githubTargetOrg = $"octoshift-e2e-bbs-{TestHelper.GetOsName()}";
+    //     var bbsProjectKey = $"IN";
+    //     var bbsRepo = "100_cli";
+    //     var bbsServer = "http://e2e-bbs-8-5-0-linux-2204.eastus.cloudapp.azure.com:7990";
+    //     var targetName = $"E2E -{TestHelper.GetOsName().ToUpper()}-e2e-{Guid.NewGuid()}";
+    //     var targetRepo = $"multi-part-{targetName}";
+
+    //     var sshKey = Environment.GetEnvironmentVariable(GetSshKeyName(bbsServer));
+    //     await File.WriteAllTextAsync(Path.Join(TestHelper.GetOsDistPath(), SSH_KEY_FILE), sshKey);
+
+
+    //     var retryPolicy = new RetryPolicy(null);
+    //     await retryPolicy.Retry(async () =>
+    //    {
+    //        await _targetHelper.ResetGithubTestEnvironment(githubTargetOrg);
+    //    });
+
+    //     var migrateRepoCommand = $"migrate-repo --bbs-server-url {bbsServer} --bbs-project {bbsProjectKey} --bbs-repo {bbsRepo} --github-org {githubTargetOrg} --github-repo{targetRepo} --ssh-user octoshift --ssh-private-key {SSH_KEY_FILE} --use-github-storage --verbose";
+
+    //     await _targetHelper.RunBbsMigrateRepoCommand(migrateRepoCommand, _tokens);
+
+    //     _targetHelper.AssertNoErrorInLogs(_startTime);
+
+    //     await _targetHelper.AssertGithubRepoExists(githubTargetOrg, targetRepo);
+    //     await _targetHelper.AssertGithubRepoInitialized(githubTargetOrg, targetRepo);
+    // }
 
     private string GetSshKeyName(string bbsServer)
     {
