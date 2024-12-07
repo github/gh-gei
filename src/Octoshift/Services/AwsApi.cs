09cbbd6c9fead9923afd07d60dcdfbc33b409189
Arin Ghazarian <aringhazarian@github.com>
2024-12-06 21:23:38 -0800

    Add progress report for uploading to AWS S3

--- src/Octoshift/Services/AwsApi.cs
+++ src/Octoshift/Services/AwsApi.cs
@@ -13,14 +13,22 @@ namespace OctoshiftCLI.Services;
 public class AwsApi : IDisposable
 {
     private const int AUTHORIZATION_TIMEOUT_IN_HOURS = 48;
+    private const int UPLOAD_PROGRESS_REPORT_INTERVAL_IN_SECONDS = 10;
 
     private readonly ITransferUtility _transferUtility;
+    private readonly object _mutex = new();
+    private readonly OctoLogger _log;
+    private DateTime _nextProgressReport = DateTime.Now;
 
-    public AwsApi(ITransferUtility transferUtility) => _transferUtility = transferUtility;
+    public AwsApi(ITransferUtility transferUtility, OctoLogger log)
+    {
+        _transferUtility = transferUtility;
+        _log = log;
+    }
 
 #pragma warning disable CA2000
-    public AwsApi(string awsAccessKeyId, string awsSecretAccessKey, string awsRegion = null, string awsSessionToken = null)
-        : this(new TransferUtility(BuildAmazonS3Client(awsAccessKeyId, awsSecretAccessKey, awsRegion, awsSessionToken)))
+    public AwsApi(OctoLogger log, string awsAccessKeyId, string awsSecretAccessKey, string awsRegion = null, string awsSessionToken = null)
+        : this(new TransferUtility(BuildAmazonS3Client(awsAccessKeyId, awsSecretAccessKey, awsRegion, awsSessionToken)), log)
 #pragma warning restore CA2000
     {
     }
@@ -51,20 +59,37 @@ public class AwsApi : IDisposable
     {
         try
         {
-            await _transferUtility.UploadAsync(fileName, bucketName, keyName);
+            var uploadRequest = new TransferUtilityUploadRequest
+            {
+                BucketName = bucketName,
+                Key = keyName,
+                FilePath = fileName
+            };
+            return await UploadToBucket(uploadRequest);
         }
         catch (Exception ex) when (ex is TaskCanceledException or TimeoutException)
         {
             throw new OctoshiftCliException($"Upload of archive \"{fileName}\" to AWS timed out", ex);
         }
-
-        return GetPreSignedUrlForFile(bucketName, keyName);
     }
 
     public virtual async Task<string> UploadToBucket(string bucketName, Stream content, string keyName)
     {
-        await _transferUtility.UploadAsync(content, bucketName, keyName);
-        return GetPreSignedUrlForFile(bucketName, keyName);
+        var uploadRequest = new TransferUtilityUploadRequest
+        {
+            BucketName = bucketName,
+            Key = keyName,
+            InputStream = content
+        };
+        return await UploadToBucket(uploadRequest);
+    }
+
+    private async Task<string> UploadToBucket(TransferUtilityUploadRequest uploadRequest)
+    {
+        uploadRequest.UploadProgressEvent += (_, args) => LogProgress(args.PercentDone, args.TransferredBytes, args.TotalBytes);
+        await _transferUtility.UploadAsync(uploadRequest);
+
+        return GetPreSignedUrlForFile(uploadRequest.BucketName, uploadRequest.Key);
     }
 
     private string GetPreSignedUrlForFile(string bucketName, string keyName)
@@ -79,6 +104,25 @@ public class AwsApi : IDisposable
         };
 
         return _transferUtility.S3Client.GetPreSignedURL(urlRequest);
+    }
+
+    private void LogProgress(int percentDone, long uploadedBytes, long totalBytes)
+    {
+        lock (_mutex)
+        {
+            if (DateTime.Now < _nextProgressReport)
+            {
+                return;
+            }
+
+            _nextProgressReport = _nextProgressReport.AddSeconds(UPLOAD_PROGRESS_REPORT_INTERVAL_IN_SECONDS);
+        }
+
+        var progressMessage = uploadedBytes > 0
+            ? $", {uploadedBytes.ToLogFriendlySize()} out of {totalBytes.ToLogFriendlySize()} ({percentDone}%) completed"
+            : "";
+
+        _log.LogInformation($"Archive upload in progress{progressMessage}...");
     }
 
     protected virtual void Dispose(bool disposing)
