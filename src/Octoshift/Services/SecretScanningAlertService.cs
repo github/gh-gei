b024a25283579933a29ed7b9f5f05fd5974fd29d
Stefan Petrushevski <theztefan@github.com>
2025-05-08 14:06:56 +0200

    fix: change var to string

--- src/Octoshift/Services/SecretScanningAlertService.cs
+++ src/Octoshift/Services/SecretScanningAlertService.cs
@@ -74,7 +74,7 @@ public class SecretScanningAlertService
 
                         _log.LogInformation($"  updating target alert:{targetAlert.Alert.Number} to state:{sourceAlert.Alert.State} and resolution:{sourceAlert.Alert.Resolution}");
 
-                        var targetResolutionComment = $"[@{sourceAlert.Alert.ResolverName}] {sourceAlert.Alert.ResolutionComment}";
+                        string targetResolutionComment = $"[@{sourceAlert.Alert.ResolverName}] {sourceAlert.Alert.ResolutionComment}";
 
                         await _targetGithubApi.UpdateSecretScanningAlert(targetOrg, targetRepo, targetAlert.Alert.Number, sourceAlert.Alert.State,
                             sourceAlert.Alert.Resolution, targetResolutionComment);
