e2178a948147e00448f4ceeffd51fdb2571115be
Maxwell Pray <synthead@github.com>
2026-05-13 16:35:52 -0700

    Use GitLab states in GL ExportState.

--- src/gl2gh/ExportState.cs
+++ src/gl2gh/ExportState.cs
@@ -2,11 +2,10 @@ namespace OctoshiftCLI.GitlabToGithub;
 
 public static class ExportState
 {
-    public const string COMPLETED = "COMPLETED";
-    public const string FAILED = "FAILED";
-    public const string ABORTED = "ABORTED";
+    public const string FINISHED = "finished";
+    public const string FAILED = "failed";
 
-    public static bool IsInProgress(string state) => state is not COMPLETED && !IsError(state);
+    public static bool IsInProgress(string state) => state is not FINISHED && !IsError(state);
 
-    public static bool IsError(string state) => state is FAILED or ABORTED;
+    public static bool IsError(string state) => state is FAILED;
 }
