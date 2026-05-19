dc467e4ebcc674738f44be5612d63376bdac4238
Briana J <brianaj@github.com>
2026-05-19 16:42:08 +0000

    delete stale tests and unused gitlabsettings

--- src/gl2gh/GitlabSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace OctoshiftCLI.GitlabToGithub;
-
-public static class GitlabSettings
-{
-    public const string DEFAULT_BBS_SHARED_HOME_DIRECTORY_WINDOWS = "c$\\atlassian\\applicationdata\\bitbucket\\shared";
-
-    public const string DEFAULT_BBS_SHARED_HOME_DIRECTORY_LINUX = "/var/atlassian/application-data/bitbucket/shared";
-}
