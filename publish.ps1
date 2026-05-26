1485885966804b4a33b13e19eef203fa9f694acf
Briana J <brianaj@github.com>
2026-05-26 22:35:41 +0000

    add integration test chanages

--- publish.ps1
+++ publish.ps1
@@ -290,3 +290,93 @@ else {
 
     Rename-Item ./dist/osx-arm64/bbs2gh bbs2gh-darwin-arm64
 }
+
+### gl2gh ###
+if ((Test-Path env:SKIP_WINDOWS) -And $env:SKIP_WINDOWS.ToUpper() -eq "TRUE") {
+    Write-Output "Skipping gl2gh Windows build because SKIP_WINDOWS is set"
+}
+else {
+    dotnet publish src/gl2gh/gl2gh.csproj -c Release -o dist/win-x64/ -r win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=partial --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true /p:VersionPrefix=$AssemblyVersion
+
+    if ($LASTEXITCODE -ne 0) {
+        exit $LASTEXITCODE
+    }
+
+    if (Test-Path -Path ./dist/win-x64/gl2gh-windows-amd64.exe) {
+        Remove-Item ./dist/win-x64/gl2gh-windows-amd64.exe
+    }
+
+    Rename-Item ./dist/win-x64/gl2gh.exe gl2gh-windows-amd64.exe
+
+    dotnet publish src/gl2gh/gl2gh.csproj -c Release -o dist/win-x86/ -r win-x86 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=partial --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true /p:VersionPrefix=$AssemblyVersion
+
+    if ($LASTEXITCODE -ne 0) {
+        exit $LASTEXITCODE
+    }
+
+    if (Test-Path -Path ./dist/win-x86/gl2gh-windows-386.exe) {
+        Remove-Item ./dist/win-x86/gl2gh-windows-386.exe
+    }
+
+    Rename-Item ./dist/win-x86/gl2gh.exe gl2gh-windows-386.exe
+}
+
+if ((Test-Path env:SKIP_LINUX) -And $env:SKIP_LINUX.ToUpper() -eq "TRUE") {
+    Write-Output "Skipping gl2gh Linux build because SKIP_LINUX is set"
+}
+else {
+    dotnet publish src/gl2gh/gl2gh.csproj -c Release -o dist/linux-x64/ -r linux-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=partial --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true /p:VersionPrefix=$AssemblyVersion
+
+    if ($LASTEXITCODE -ne 0) {
+        exit $LASTEXITCODE
+    }
+
+    if (Test-Path -Path ./dist/linux-x64/gl2gh-linux-amd64) {
+        Remove-Item ./dist/linux-x64/gl2gh-linux-amd64
+    }
+
+    Rename-Item ./dist/linux-x64/gl2gh gl2gh-linux-amd64
+
+    # linux-arm64 build
+    dotnet publish src/gl2gh/gl2gh.csproj -c Release -o dist/linux-arm64/ -r linux-arm64 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=partial --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true /p:VersionPrefix=$AssemblyVersion
+
+    if ($LASTEXITCODE -ne 0) {
+        exit $LASTEXITCODE
+    }
+
+    if (Test-Path -Path ./dist/linux-arm64/gl2gh-linux-arm64) {
+        Remove-Item ./dist/linux-arm64/gl2gh-linux-arm64
+    }
+
+    Rename-Item ./dist/linux-arm64/gl2gh gl2gh-linux-arm64
+}
+
+if ((Test-Path env:SKIP_MACOS) -And $env:SKIP_MACOS.ToUpper() -eq "TRUE") {
+    Write-Output "Skipping gl2gh MacOS build because SKIP_MACOS is set"
+}
+else {
+    dotnet publish src/gl2gh/gl2gh.csproj -c Release -o dist/osx-x64/ -r osx-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=partial --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true /p:VersionPrefix=$AssemblyVersion
+
+    if ($LASTEXITCODE -ne 0) {
+        exit $LASTEXITCODE
+    }
+
+    if (Test-Path -Path ./dist/osx-x64/gl2gh-darwin-amd64) {
+        Remove-Item ./dist/osx-x64/gl2gh-darwin-amd64
+    }
+
+    Rename-Item ./dist/osx-x64/gl2gh gl2gh-darwin-amd64
+
+    # osx-arm64 build
+    dotnet publish src/gl2gh/gl2gh.csproj -c Release -o dist/osx-arm64/ -r osx-arm64 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:TrimMode=partial --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true /p:VersionPrefix=$AssemblyVersion
+
+    if ($LASTEXITCODE -ne 0) {
+        exit $LASTEXITCODE
+    }
+
+    if (Test-Path -Path ./dist/osx-arm64/gl2gh-darwin-arm64) {
+        Remove-Item ./dist/osx-arm64/gl2gh-darwin-arm64
+    }
+
+    Rename-Item ./dist/osx-arm64/gl2gh gl2gh-darwin-arm64
+}
